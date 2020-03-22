69c1d580fd812ae9a5619cb5f6642dcbbd1a1b19
C. R. Ayres <ca469@kent.ac.uk>
2020-03-22 16:47:42 +0000

    Portable update message, and online tab name fix

---
+++ src/_managerinf/ManagerInformation.ts
@@ -0,0 +1,6 @@
+import VersionNumber from 'src/model/VersionNumber';
+
+export default class ManagerInformation {
+    public static VERSION: VersionNumber = new VersionNumber('3.0.10');
+    public static IS_PORTABLE: boolean = false;
+}
