e5371d9b7363440b9df85dc46aeb9b460889ca3f
Cade Ayres <ca469@kent.ac.uk>
2020-07-07 17:55:58 +0100

    Improved test example

--- tests/test-setup.test.ts
+++ tests/test-setup.test.ts
@@ -1,12 +1,14 @@
 import 'mocha';
 import PathResolver from '../src/r2mm/manager/PathResolver';
 import * as fs from 'fs-extra';
+import Profile from '../src/model/Profile';
 
-before(async () => {
+before(() => {
     PathResolver.ROOT = '__test_data__';
+    new Profile("Default");
 });
 
-after(async () => {
+after(() => {
     fs.emptyDirSync(PathResolver.ROOT);
     fs.removeSync(PathResolver.ROOT);
 });
--- tests/unit/data/LogOutput.test.ts
+++ tests/unit/data/LogOutput.test.ts
@@ -2,11 +2,11 @@ import 'mocha';
 import { expect } from 'chai';
 import LogOutput from '../../../src/r2mm/data/LogOutput';
 import Profile from '../../../src/model/Profile';
+import '../../test-setup.test';
 
 describe('LogOutput', () => {
 
     it('Singletons should be the same object', () => {
-        new Profile("default");
         expect(LogOutput.getSingleton()).equals(LogOutput.getSingleton());
     });
 
