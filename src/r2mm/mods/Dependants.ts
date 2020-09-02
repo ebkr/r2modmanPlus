2e13a5bb40b7175052a74de04faed9c2c9f7269c
Cade Ayres <ca469@kent.ac.uk>
2020-09-02 15:29:38 +0100

    Fixed naming match issue in Dependants.ts

--- src/r2mm/mods/Dependants.ts
+++ src/r2mm/mods/Dependants.ts
@@ -4,11 +4,16 @@ export default class Dependants {
 
     public static getDependantList(manifestMod: ManifestV2, modList: ManifestV2[]): Set<ManifestV2> {
         const dependants = new Set<ManifestV2>();
-        modList.forEach(dependant => {
-            dependant.getDependencies().forEach(mod => {
-                if (mod.startsWith(manifestMod.getName())) {
-                    dependants.add(dependant);
-                    this.getDependantList(dependant, modList).forEach(recursive => {
+
+        // For all mods
+        modList.forEach(mod => {
+            // Get mod dependencies
+            mod.getDependencies().forEach(dependency => {
+                // If mod name equals mod trying to get dependants
+                if (dependency.startsWith(manifestMod.getName() + "-")) {
+                    dependants.add(mod);
+                    // Get dependants of new mod
+                    this.getDependantList(mod, modList).forEach(recursive => {
                         dependants.add(recursive);
                     })
                 }
@@ -21,7 +26,7 @@ export default class Dependants {
         const dependencies = new Set<ManifestV2>();
         manifestMod.getDependencies().forEach(dependant => {
             modList.forEach(mod => {
-                if (dependant.startsWith(mod.getName())) {
+                if (dependant.startsWith(mod.getName() + "-")) {
                     dependencies.add(mod);
                     this.getDependencyList(mod, modList).forEach(recursive => {
                         dependencies.add(recursive);
