38ef26e6d13d840f2c6813a7c3b7ded448b4b26b
Mackenzie McClane <yuudaari@gmail.com>
2020-09-11 02:13:07 +1000

    Import bulma via app.scss rather than via min.css file

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,8 @@ import Vue from 'vue';
 import Component from 'vue-class-component';
 import * as fs from 'fs-extra';
 import { ipcRenderer } from 'electron';
-import 'bulma/css/bulma.css';
 // import 'src/css/superhero.bulma.css';
-import 'bulma-steps/dist/css/bulma-steps.min.css';
 import 'bulma-steps/dist/js/bulma-steps.min.js';
-import 'bulma-divider/dist/css/bulma-divider.min.css';
-import 'bulma-tooltip/dist/css/bulma-tooltip.min.css';
 import { isNull } from 'util';
 
 import * as path from 'path';
