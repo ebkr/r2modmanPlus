5f09c49e094187e99e337485ad0f18c28b824f44
Cade Ayres <ca469@kent.ac.uk>
2020-09-16 23:30:48 +0100

    Added Quill editor for non .cfg files. Fixed tooltip wrapping.

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,7 @@
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
-import * as fs from 'fs-extra';
-import { ipcRenderer } from 'electron';
-// import 'src/css/superhero.bulma.css';
 import 'bulma-steps/dist/js/bulma-steps.min.js';
-import { isNull } from 'util';
-
-import * as path from 'path';
 
 @Component
 export default class App extends Vue {
