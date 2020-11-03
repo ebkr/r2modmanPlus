7d1b6cc289515ba01ebeb8cc52c1aef4af515999
Cade Ayres <ca469@kent.ac.uk>
2020-11-03 22:00:20 +0000

    Updated README and versions

--- src/_managerinf/ManagerInformation.ts
+++ src/_managerinf/ManagerInformation.ts
@@ -1,6 +1,6 @@
 import VersionNumber from '../model/VersionNumber';
 
 export default class ManagerInformation {
-    public static VERSION: VersionNumber = new VersionNumber('3.0.35');
+    public static VERSION: VersionNumber = new VersionNumber('3.0.36');
     public static IS_PORTABLE: boolean = false;
 }
