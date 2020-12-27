b5143757b856edb576032d8fe81b20722f240501
Cade Ayres <ca469@kent.ac.uk>
2020-12-27 13:11:57 +0000

    Replaced spaces with margins and grid with flex for nav

--- src/components/navigation/NavigationMenu.vue
+++ src/components/navigation/NavigationMenu.vue
@@ -15,10 +15,10 @@
         <aside class="menu">
             <p class="menu-label">Risk of Rain 2</p>
             <ul class="menu-list">
-                <li><a href="#" @click="launchModded"><i class="fas fa-play-circle"/>&nbsp;&nbsp;Start modded</a>
+                <li><a href="#" @click="launchModded"><i class="fas fa-play-circle icon--margin-right"/>Start modded</a>
                 </li>
                 <li>
-                    <a href="#" @click="launchVanilla"><i class="far fa-play-circle"/>&nbsp;&nbsp;Start
+                    <a href="#" @click="launchVanilla"><i class="far fa-play-circle icon--margin-right"/>Start
                         vanilla</a>
                 </li>
             </ul>
@@ -29,18 +29,18 @@
                     <!-- Due to this, the click event must be applied to all children. Parent container also binds click to account for margins. -->
                     <a href="#" data-ref="installed" @click="emitClick($event.target)"
                        class="tagged-link" :class="[view === 'installed' ? 'is-active' : '']">
-                        <i class="fas fa-folder tagged-link__icon" data-ref="installed" @click="emitClick($event.target)"/>
-                        <span data-ref="installed" @click="emitClick($event.target)">Installed</span>
-                        <span class="tag" :class="[{'is-link': view !== 'installed'}]"
+                        <i class="fas fa-folder tagged-link__icon icon--margin-right" data-ref="installed" @click="emitClick($event.target)"/>
+                        <span class="tagged-link__content" data-ref="installed" @click="emitClick($event.target)">Installed</span>
+                        <span class="tag tagged-link__tag" :class="[{'is-link': view !== 'installed'}]"
                         data-ref="installed" @click="emitClick($event.target)">{{localModList.length}}</span>
                     </a>
                 </li>
                 <li>
                     <a href="#" data-ref="online" @click="emitClick($event.target)"
                        class="tagged-link" :class="[view === 'online' ? 'is-active' : '']">
-                        <i class="fas fa-globe tagged-link__icon" data-ref="online" @click="emitClick($event.target)"/>
-                        <span data-ref="online" @click="emitClick($event.target)">Online</span>
-                        <span class="tag" :class="[{'is-link': view !== 'online'}]"
+                        <i class="fas fa-globe tagged-link__icon icon--margin-right" data-ref="online" @click="emitClick($event.target)"/>
+                        <span class="tagged-link__content" data-ref="online" @click="emitClick($event.target)">Online</span>
+                        <span class="tag tagged-link__tag" :class="[{'is-link': view !== 'online'}]"
                               data-ref="online" @click="emitClick($event.target)">{{thunderstoreModList.length}}</span>
                     </a>
                 </li>
@@ -49,42 +49,42 @@
             <ul class='menu-list'>
                 <li>
                     <a href="#" :class="[view === 'config-editor' ? 'is-active' : '']" data-ref="config-editor" @click="emitClick($event.target)">
-                        <i class="fas fa-edit"/>&nbsp;&nbsp;Config editor
+                        <i class="fas fa-edit icon--margin-right"/>Config editor
                     </a>
                 </li>
                 <li>
                     <a href="#" :class="[view === 'settings' ? 'is-active' : '']"
                        data-ref="settings" @click="emitClick($event.target)">
-                        <i class="fas fa-cog"/>&nbsp;&nbsp;Settings
+                        <i class="fas fa-cog icon--margin-right"/>Settings
                     </a>
                 </li>
                 <li>
                     <a href="#" :class="[view === 'help' ? 'is-active' : '']"
                        data-ref="help" @click="emitClick($event.target)">
-                        <i class="fas fa-question-circle"/>&nbsp;&nbsp;Help</a>
+                        <i class="fas fa-question-circle icon--margin-right"/>Help</a>
                     <ul v-if="view === 'help'">
                         <li>
                             <a href='#' :class="[{'is-active': helpPage === 'tips-and-tricks'}]"
                                data-ref="tips-and-tricks" @click="emitHelpSectionClick($event.target)">
-                                <i class="fas fa-lightbulb"/>&nbsp;&nbsp;Tips and tricks
+                                <i class="fas fa-lightbulb icon--margin-right"/>Tips and tricks
                             </a>
                         </li>
                         <li>
                             <a href='#' :class="[{'is-active': helpPage === 'game-wont-start'}]"
                                data-ref="game-wont-start" @click="emitHelpSectionClick($event.target)">
-                                <i class="fas fa-gamepad"/>&nbsp;&nbsp;Game won't start
+                                <i class="fas fa-gamepad icon--margin-right"/>Game won't start
                             </a>
                         </li>
                         <li>
                             <a href='#' :class="[{'is-active': helpPage === 'mods-not-working'}]"
                                data-ref="mods-not-working" @click="emitHelpSectionClick($event.target)">
-                                <i class="fas fa-ban"/>&nbsp;&nbsp;Mods aren't working
+                                <i class="fas fa-ban icon--margin-right"/>Mods aren't working
                             </a>
                         </li>
                         <li>
                             <a href='#' :class="[{'is-active': helpPage === 'like-r2'}]"
                                data-ref="like-r2" @click="emitHelpSectionClick($event.target)">
-                                <i class="fas fa-heart"/>&nbsp;&nbsp;Like {{ appName }}?
+                                <i class="fas fa-heart icon--margin-right"/>Like {{ appName }}?
                             </a>
                         </li>
                     </ul>
