5035fe42e71deb9498bcca9288fbbdb066d17208
Naomi Calabretta <me@arytonex.pw>
2021-02-27 01:43:28 +0100

    LNGS: Make hero a warning type

--- src/pages/LinuxNativeGameSetup.vue
+++ src/pages/LinuxNativeGameSetup.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<hero title="Getting started on Linux" subtitle="Let's configure the game properly" heroType="is-info" />
+		<hero title="Getting started on Linux" subtitle="Let's configure the game properly" heroType="is-warning" />
 		<br/>
 		<div class="container">
 			To be able to launch {{ activeGame }} on Linux, you must first setup your Steam launch options correctly.<br/>
