5eb440f35a70bd9443a5781ab5fd026330f73de7
Cade Ayres <ca469@kent.ac.uk>
2021-03-04 00:41:15 +0000

    Added fallback target to use BepInEx bleeding edge preloader

--- src/providers/generic/game/win32/GameRunnerProviderImpl.ts
+++ src/providers/generic/game/win32/GameRunnerProviderImpl.ts
@@ -7,43 +7,44 @@ import path from "path";
 import Profile from '../../../../model/Profile';
 import GameDirectoryResolverProvider from '../../../ror2/game/GameDirectoryResolverProvider';
 import Game from '../../../../model/game/Game';
+import FsProvider from 'src/providers/generic/file/FsProvider';
 
 export default class GameRunnerProviderImpl extends GameRunnerProvider {
 
     async startModded(game: Game): Promise<void | R2Error> {
-        return new Promise(async (resolve, reject) => {
+        return new Promise(async () => {
             LoggerProvider.instance.Log(LogSeverity.INFO, 'Launching modded');
-            const settings = await ManagerSettings.getSingleton(game);
-            const steamDir: string | R2Error = await GameDirectoryResolverProvider.instance.getSteamDirectory();
-            if (steamDir instanceof R2Error) {
-                return reject(steamDir);
+            let preloaderPath: string;
+            if (await FsProvider.instance.exists(path.join(Profile.getActiveProfile().getPathOfProfile(), "BepInEx", "core", "BepInEx.Preloader.dll"))) {
+                // BepInEx Standard
+                preloaderPath = path.join(Profile.getActiveProfile().getPathOfProfile(), "BepInEx", "core", "BepInEx.Preloader.dll");
+            } else {
+                // BepInEx Bleeding Edge
+                preloaderPath = path.join(Profile.getActiveProfile().getPathOfProfile(), "BepInEx", "core", "BepInEx.Preloader.Core.dll");
             }
-            LoggerProvider.instance.Log(LogSeverity.INFO, `Steam directory is: ${steamDir}`);
-            LoggerProvider.instance.Log(LogSeverity.INFO,
-                `Running command: ${steamDir}.exe -applaunch ${game.appId} --doorstop-enable true --doorstop-target "${path.join(Profile.getActiveProfile().getPathOfProfile(), "BepInEx", "core", "BepInEx.Preloader.dll")}" ${settings.getContext().gameSpecific.launchParameters}`);
-            exec(`"${steamDir}/Steam.exe" -applaunch ${game.appId} --doorstop-enable true --doorstop-target "${path.join(Profile.getActiveProfile().getPathOfProfile(), "BepInEx", "core", "BepInEx.Preloader.dll")}" ${settings.getContext().gameSpecific.launchParameters}`, (err => {
-                if (err !== null) {
-                    LoggerProvider.instance.Log(LogSeverity.ACTION_STOPPED, 'Error was thrown whilst starting modded');
-                    LoggerProvider.instance.Log(LogSeverity.ERROR, err.message);
-                    const r2err = new R2Error('Error starting Steam', err.message, 'Ensure that the Steam directory has been set correctly in the settings');
-                    return reject(r2err);
-                }
-                return resolve();
-            }));
+            return this.start(game, `--doorstop-enable true --doorstop-target "${preloaderPath}"`);
         });
     }
 
     async startVanilla(game: Game): Promise<void | R2Error> {
         return new Promise(async (resolve, reject) => {
             LoggerProvider.instance.Log(LogSeverity.INFO, 'Launching vanilla');
+            return this.start(game, `--doorstop-enable false`);
+        });
+    }
+
+    async start(game: Game, args: string) {
+        return new Promise(async (resolve, reject) => {
             const settings = await ManagerSettings.getSingleton(game);
-            const steamDir: string | R2Error = await GameDirectoryResolverProvider.instance.getSteamDirectory();
+            const steamDir = await GameDirectoryResolverProvider.instance.getSteamDirectory();
             if (steamDir instanceof R2Error) {
-                return reject(steamDir);
+                return steamDir;
             }
+
             LoggerProvider.instance.Log(LogSeverity.INFO, `Steam directory is: ${steamDir}`);
-            LoggerProvider.instance.Log(LogSeverity.INFO, `Running command: ${steamDir}.exe -applaunch ${game.appId} --doorstop-enable false`);
-            exec(`"${steamDir}/Steam.exe" -applaunch ${game.appId} --doorstop-enable false ${settings.getContext().gameSpecific.launchParameters}`, (err => {
+            LoggerProvider.instance.Log(LogSeverity.INFO, `Running command: ${steamDir}.exe -applaunch ${game.appId} ${args} ${settings.getContext().gameSpecific.launchParameters}`);
+
+            exec(`"${steamDir}/Steam.exe" -applaunch ${game.appId} ${args} ${settings.getContext().gameSpecific.launchParameters}`, (err => {
                 if (err !== null) {
                     LoggerProvider.instance.Log(LogSeverity.ACTION_STOPPED, 'Error was thrown whilst starting modded');
                     LoggerProvider.instance.Log(LogSeverity.ERROR, err.message);
