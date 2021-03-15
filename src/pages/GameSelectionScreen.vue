243a6651f2c8195f4eac68f73d2c5e55a3822860
Cade Ayres <ca469@kent.ac.uk>
2021-03-15 22:27:36 +0000

    Reduced image size and added a favourite action icon to the cards

--- src/pages/GameSelectionScreen.vue
+++ src/pages/GameSelectionScreen.vue
@@ -31,41 +31,32 @@
                                 </div>
                                 <br/>
 
-                                <div class="text-center">
+                                <div>
                                     <div v-for="(game, index) of filteredGameList" :key="`${index}-${game.displayName}-${selectedGame === game}`" class="inline-block margin-right margin-bottom">
 
-                                        <div class="border-at-bottom inline" @click="selectGame(game)">
+                                        <div class="inline">
                                             <div class='card is-shadowless'>
                                                 <div class='cursor-pointer'>
                                                     <header class='card-header is-shadowless is-relative'>
                                                         <div class="absolute-full z-top flex">
-                                                            <div class="card-action-overlay rounded">
-                                                                <div class="absolute-top" :class="['card-header-title']">
+                                                            <a href="#" class="card-action-overlay rounded">
+                                                                <div class="absolute-top-right card-header-title">
+                                                                    <p class="text-left title is-5">
+                                                                        <i class="fas fa-star text-warning" v-if="favourites.includes(game.internalFolderName)"></i>
+                                                                        <i class="far fa-star" v-else></i>
+                                                                    </p>
+                                                                </div>
+                                                                <div class="absolute-top card-header-title">
                                                                     <p class="text-left title is-5">{{ game.displayName }}</p>
                                                                 </div>
-                                                                <div class="absolute-center" v-if="selectedGame === game && flow === 2">
-                                                                    <div v-for="(platformMetadata) of game.storePlatformMetadata"
-                                                                         :key="platformMetadata.storePlatform.toString()"
-                                                                         class="text-left">
-                                                                        <input :id="`${game.internalFolderName}-platform-${platformMetadata.storePlatform.toString()}`"
-                                                                               name="platform" class="radio" type="radio"/>
-                                                                        <label :for="`${game.internalFolderName}-platform-${platformMetadata.storePlatform.toString()}`">
-                                                                            {{ platformMetadata.storePlatform }}
-                                                                        </label>
-                                                                    </div>
-                                                                    <br/>
-                                                                    <button class="button is-info">Continue</button>
-                                                                    <br/><br/>
-                                                                    <button class="button">Go back</button>
-                                                                </div>
-                                                                <div class="absolute-center" v-else>
-                                                                    <button class="button is-info">Select game</button>
+                                                                <div class="absolute-center">
+                                                                    <button class="button is-info" @click="selectGame(game)">Select game</button>
                                                                     <br/><br/>
                                                                     <button class="button">Set as default</button>
                                                                 </div>
-                                                            </div>
+                                                            </a>
                                                         </div>
-                                                        <div class="image is-fullwidth">
+                                                        <div class="image is-fullwidth border border--top border--border-box rounded" :class="[{'border--warning': favourites.includes(game.internalFolderName)}]">
                                                             <img :src='`/images/game_selection/${game.gameImage}`' alt='Mod Logo' class="rounded"/>
                                                         </div>
                                                     </header>
@@ -95,6 +86,7 @@ import PathResolver from '../r2mm/manager/PathResolver';
 import * as path from 'path';
 import FileUtils from '../utils/FileUtils';
 import ManagerSettings from '../r2mm/manager/ManagerSettings';
+import { StorePlatform } from 'src/model/game/StorePlatform';
 
 @Component({
     components: {
@@ -106,22 +98,36 @@ export default class GameSelectionScreen extends Vue {
     private runningMigration = false;
     private selectedGame: Game | null = null;
     private filterText: string = "";
-    private flow: int = 1;
+    private showPlatformModal: boolean = false;
+    private selectedPlatform: StorePlatform | undefined;
+    private favourites: string[] = ["RiskOfRain2"];
 
     get filteredGameList() {
         return this.gameList.filter(value => value.displayName.toLowerCase().indexOf(this.filterText.toLowerCase()) >= 0 || this.filterText.trim().length === 0);
     }
 
     get gameList(): Game[] {
-        return GameManager.gameList;
+        return GameManager.gameList.sort((a, b) => {
+            if (this.favourites.includes(a.internalFolderName)) {
+                if (this.favourites.includes(b.internalFolderName)) {
+                    return a.displayName.toLowerCase().localeCompare(b.displayName.toLowerCase());
+                } else {
+                    return -1;
+                }
+            } else if (this.favourites.includes(b.internalFolderName)) {
+                return 1;
+            }
+            return a.displayName.toLowerCase().localeCompare(b.displayName.toLowerCase());
+        });
     }
 
     private selectGame(game: Game) {
         this.selectedGame = game;
         if (game.storePlatformMetadata.length > 1) {
-            this.flow = 2;
+            this.showPlatformModal = true;
         } else {
-            this.flow = 1;
+            this.showPlatformModal = false;
+            this.proceed();
         }
     }
 
