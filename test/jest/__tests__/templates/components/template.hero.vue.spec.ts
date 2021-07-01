3c9c91a9290a68f98e765509f3bd4433575a8abe
Cade Ayres <ca469@kent.ac.uk>
2021-07-01 20:04:44 +0100

    Added Hero template test

---
+++ test/jest/__tests__/templates/components/template.hero.vue.spec.ts
@@ -0,0 +1,47 @@
+import TestSetup from '../../test-setup';
+import { shallowMount } from '@vue/test-utils';
+import Hero from '../../../../../src/components/Hero.vue';
+
+type MountOptions = {
+    propsData: {
+        title: string,
+        heroType: string,
+        subtitle: string
+    }
+};
+
+describe("Hero component", () => {
+
+
+    let mountOptions: MountOptions;
+
+    beforeEach(() => {
+        TestSetup.stubSetUp();
+        mountOptions = {
+            propsData: {
+                title: "Some title",
+                heroType: "Hero Type",
+                subtitle: "Some subtitle"
+            }
+        }
+    });
+
+    it("Has title text", () => {
+        const mount = shallowMount(Hero, mountOptions);
+        const found = mount.findComponent({ ref: "title"});
+        expect(found.text()).toBe(mountOptions.propsData.title);
+    });
+
+    it("Has subtitle text", () => {
+        const mount = shallowMount(Hero, mountOptions);
+        const found = mount.findComponent({ ref: "subtitle"});
+        expect(found.text()).toBe(mountOptions.propsData.subtitle);
+    });
+
+    it("Has correct class", () => {
+        const mount = shallowMount(Hero, mountOptions);
+        const found = mount.findComponent({ ref: "section"});
+        expect(found.classes().join(" ")).toBe(`hero ${mountOptions.propsData.heroType}`);
+    });
+
+});
