55f721e2e04fcdbe116c7e12cb71bfa8acc9a425
Cade Ayres <ca469@kent.ac.uk>
2021-08-06 14:06:10 +0100

    Updated imports to relative for NoResolutionConflictManagement

--- src/r2mm/installing/NoResolutionConflictManagement.ts
+++ src/r2mm/installing/NoResolutionConflictManagement.ts
@@ -1,7 +1,7 @@
-import ConflictManagementProvider from 'src/providers/generic/installing/ConflictManagementProvider';
-import ManifestV2 from 'src/model/ManifestV2';
-import Profile from 'src/model/Profile';
-import R2Error from 'src/model/errors/R2Error';
+import ConflictManagementProvider from '../../providers/generic/installing/ConflictManagementProvider';
+import ManifestV2 from '../../model/ManifestV2';
+import Profile from '../../model/Profile';
+import R2Error from '../../model/errors/R2Error';
 
 export default class NoResolutionConflictManagement extends ConflictManagementProvider {
     async isFileActive(mod: ManifestV2, profile: Profile, file: string): Promise<R2Error | boolean> {
