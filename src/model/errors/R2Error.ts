f34749729ba71109af1287efc09cab9a93c08c66
Cade Ayres <ca469@kent.ac.uk>
2021-09-06 17:38:37 +0100

    Added error reference for later use

--- src/model/errors/R2Error.ts
+++ src/model/errors/R2Error.ts
@@ -3,6 +3,7 @@ export default class R2Error extends Error {
     public message: string;
     public stack?: string | undefined;
     public solution: string;
+    public errorReferenceString: string | undefined;
 
     public constructor(name: string, message: string, solution: string | null) {
         super(message);
