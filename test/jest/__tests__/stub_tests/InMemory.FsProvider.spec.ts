6898b8c92e020b859abb8e8c639b3cbf327e0029
Cade Ayres <ca469@kent.ac.uk>
2021-09-26 23:29:09 +0100

    Added in-memory FS provider implementation

---
+++ test/jest/__tests__/stub_tests/InMemory.FsProvider.spec.ts
@@ -0,0 +1,42 @@
+import FsProvider from '../../../../src/providers/generic/file/FsProvider';
+import InMemoryFsProvider from '../stubs/providers/InMemory.FsProvider';
+import * as path from 'path';
+
+describe("InMemoryFsProvider", () => {
+
+    beforeEach(() => {
+        FsProvider.provide(() => new InMemoryFsProvider());
+        InMemoryFsProvider.clear();
+    });
+
+    test("Exists and Mkdirs", async () => {
+        const existsBefore = await FsProvider.instance.exists("Test");
+        await FsProvider.instance.mkdirs("Test");
+        const existsAfter = await FsProvider.instance.exists("Test");
+        expect(existsBefore).toBeFalsy();
+        expect(existsAfter).toBeTruthy();
+    })
+
+    test("WriteFile and ReadFile", async () => {
+        await FsProvider.instance.mkdirs("Test");
+        const existsBefore = await FsProvider.instance.exists(path.join("Test", "TestWriteFile"));
+        await FsProvider.instance.writeFile(path.join("Test", "TestWriteFile"), "test_content");
+        const existsAfter = await FsProvider.instance.exists(path.join("Test", "TestWriteFile"));
+        const content = await FsProvider.instance.readFile(path.join("Test", "TestWriteFile"));
+        expect(existsBefore).toBeFalsy();
+        expect(existsAfter).toBeTruthy();
+        expect(content.toString()).toBe("test_content");
+    });
+
+    test("WriteFile and Exists", async () => {
+        await FsProvider.instance.mkdirs("Test");
+        const existsBefore = await FsProvider.instance.exists(path.join("Test", "TestWriteFile"));
+        await FsProvider.instance.writeFile(path.join("Test", "TestWriteFile"), "test_content");
+        const existsAfter = await FsProvider.instance.exists(path.join("Test", "TestWriteFile"));
+        const content = await FsProvider.instance.readFile(path.join("Test", "TestWriteFile"));
+        expect(existsBefore).toBeFalsy();
+        expect(existsAfter).toBeTruthy();
+        expect(content.toString()).toBe("test_content");
+    });
+
+});
