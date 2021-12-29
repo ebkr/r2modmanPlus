b42f23d4f2c793a1e638846a8bee33fbcda54065
Cade Ayres <ca469@kent.ac.uk>
2021-12-29 17:43:25 +0000

    Game directory for EGS games can now be automatically detected on Windows

--- src/providers/generic/game/directory_resolver/win/EGSDirectoryResolver.ts
+++ src/providers/generic/game/directory_resolver/win/EGSDirectoryResolver.ts
@@ -2,16 +2,48 @@ import GameDirectoryResolverProvider from '../../../../ror2/game/GameDirectoryRe
 import Game from '../../../../../model/game/Game';
 import R2Error from '../../../../../model/errors/R2Error';
 import ManagerSettings from '../../../../../r2mm/manager/ManagerSettings';
+import FsProvider from 'src/providers/generic/file/FsProvider';
+import * as path from 'path';
+import EgsInstallationListItem from 'src/providers/generic/game/directory_resolver/win/EgsInstallationListItem';
 
 export default class EGSDirectoryResolver extends GameDirectoryResolverProvider {
 
     public async getDirectory(game: Game): Promise<string | R2Error> {
         const settings = await ManagerSettings.getSingleton(game);
-        return settings.getContext().gameSpecific.gameDirectory ||  new R2Error(
+        if (settings.getContext().gameSpecific.gameDirectory !== null) {
+            return settings.getContext().gameSpecific.gameDirectory!;
+        }
+        /*
+        EGS maintains a list of installed apps in:
+        C:\ProgramData\Epic\UnrealEngineLauncher\LauncherInstalled.dat
+
+        Unsure if there's a way to pull this information from the registry.
+        */
+        const datFilePath = path.join("C:", "ProgramData", "Epic", "UnrealEngineLauncher", "LauncherInstalled.dat");
+        if (await FsProvider.instance.exists(datFilePath)) {
+            try {
+                const datBuffer = JSON.parse((await FsProvider.instance.readFile(datFilePath)).toString());
+                const installationList = datBuffer.InstallationList as EgsInstallationListItem[];
+                const gameMatch = installationList.find(value => value.AppName === game.activePlatform.storeIdentifier);
+                if (gameMatch !== undefined) {
+                    if (await FsProvider.instance.exists(gameMatch.InstallLocation)) {
+                        return gameMatch.InstallLocation;
+                    }
+                }
+            } catch (e) {
+                return new R2Error(
+                    `EGS LauncherInstalled.dat file is not valid JSON`,
+                    "",
+                    `Try manually locating the ${game.displayName} install directory through the settings`
+                )
+            }
+
+        }
+        return new R2Error(
             `Unable to resolve the ${game.displayName} install directory`,
             "",
             `Try manually locating the ${game.displayName} install directory through the settings`
-        )
+        );
     }
 
     public async getSteamDirectory(): Promise<string | R2Error> {
---
+++ src/providers/generic/game/directory_resolver/win/EgsInstallationListItem.ts
@@ -0,0 +1,8 @@
+export default interface EgsInstallationListItem {
+    InstallLocation: string
+    NamespaceId: string
+    ItemId: string
+    ArtifactId: string
+    AppVersion: string
+    AppName: string
+}
