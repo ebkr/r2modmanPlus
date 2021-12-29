c455b8878504fea315403ba5bf66a990ce89862e
Cade Ayres <ca469@kent.ac.uk>
2021-12-29 17:49:05 +0000

    Added commas to interface items

--- src/providers/generic/game/directory_resolver/win/EgsInstallationListItem.ts
+++ src/providers/generic/game/directory_resolver/win/EgsInstallationListItem.ts
@@ -1,8 +1,8 @@
 export default interface EgsInstallationListItem {
-    InstallLocation: string
-    NamespaceId: string
-    ItemId: string
-    ArtifactId: string
-    AppVersion: string
+    InstallLocation: string,
+    NamespaceId: string,
+    ItemId: string,
+    ArtifactId: string,
+    AppVersion: string,
     AppName: string
 }
