88b199d91883a4ba70b73b618c2de993642dd39d
Antti Mäki <antti@anttimaki.com>
2022-11-22 13:51:23 +0200

    Add standalone CategoryFilterModal
    Continue making Manager page less of a god-class component.
    
    This also partially fixes the problem where "allow NSFW" checkbox
    didn't react correctly to dark/light theme. The value is now read from
    correct place, so the checkbox is themed based on the theme that was
    active when user entered Manager page. However, if user changes the
    theme while on manager page, the checkbox theme is not updated yet. I
    wasn't sure how this should be solved, and it falls outside the scope
    of the ticket, so I just marked the issue with TODO comment.
    
    Refs TS-1309

--- src/store/modules/ModalsModule.ts
+++ src/store/modules/ModalsModule.ts
@@ -1,15 +1,25 @@
 interface State {
+    isCategoryFilterModalOpen: boolean;
     isGameRunningModalOpen: boolean;
 }
 
 export default {
     state: (): State => ({
+        isCategoryFilterModalOpen: false,
         isGameRunningModalOpen: false,
     }),
 
     mutations: {
+        closeCategoryFilterModal: function(state: State): void {
+            state.isCategoryFilterModalOpen = false;
+        },
+
         closeGameRunningModal: function(state: State): void {
             state.isGameRunningModalOpen = false;
+        },
+
+        openCategoryFilterModal: function(state: State): void {
+            state.isCategoryFilterModalOpen = true;
         },
 
         openGameRunningModal: function(state: State): void {
