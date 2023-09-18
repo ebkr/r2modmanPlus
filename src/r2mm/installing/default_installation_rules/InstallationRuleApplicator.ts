e58617b2a58c7e5f7626eae1111b43462a620389
NikoTheFox <31776319+RandomWolf@users.noreply.github.com>
2023-09-18 13:05:54 +0200

    Update InstallationRuleApplicator.ts
    Added Wizard With a Gun

--- src/r2mm/installing/default_installation_rules/InstallationRuleApplicator.ts
+++ src/r2mm/installing/default_installation_rules/InstallationRuleApplicator.ts
@@ -108,6 +108,7 @@ export default class InstallationRuleApplicator {
             buildBepInExRules("WizardOfLegend"),
             buildBepInExRules("BombRushCyberfunk"),
             buildBepInExRules("TouhouLostBranchOfLegend"),
+            buildBepInExRules("WizardWithAGun"),
         ]
     }
 }
