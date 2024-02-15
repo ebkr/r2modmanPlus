359f57eb6e6570983901dc51170a3a8f30bf4381
Antti Mäki <antti@anttimaki.com>
2024-02-15 14:38:44 +0200

    ProfileModule: add support for disabling mods via Vuex store
    The implementation imitates what's currently being done by
    LocalModList.performDisable().
    
    This helps remove business logic and driect references to providers
    from components, hopefully supporting reuse and easier refactoring.

--- src/store/modules/ProfileModule.ts
+++ src/store/modules/ProfileModule.ts
@@ -1,11 +1,16 @@
-import { GetterTree } from 'vuex';
+import { ActionTree, GetterTree } from 'vuex';
 
 import { State as RootState } from '../index';
+import R2Error from '../../model/errors/R2Error';
 import ManifestV2 from '../../model/ManifestV2';
+import Profile from "../../model/Profile";
 import { SortDirection } from '../../model/real_enums/sort/SortDirection';
 import { SortLocalDisabledMods } from '../../model/real_enums/sort/SortLocalDisabledMods';
 import { SortNaming } from '../../model/real_enums/sort/SortNaming';
+import ConflictManagementProvider from '../../providers/generic/installing/ConflictManagementProvider';
+import ProfileInstallerProvider from '../../providers/ror2/installing/ProfileInstallerProvider';
 import ModListSort from '../../r2mm/mods/ModListSort';
+import ProfileModList from '../../r2mm/mods/ProfileModList';
 import SearchUtils from '../../utils/SearchUtils';
 
 interface State {
@@ -85,6 +90,80 @@ export default {
 
         setSearchQuery(state: State, value: string) {
             state.searchQuery = value.trim();
+        },
+    },
+
+    actions: <ActionTree<State, RootState>>{
+        async disableModsFromActiveProfile(
+            {dispatch},
+            params: {
+                mods: ManifestV2[],
+                onProgress?: (mod: ManifestV2) => void,
+            }
+        ) {
+            // TODO: manage active profile in Vuex.
+            const profile = Profile.getActiveProfile();
+
+            // Sanity check.
+            if (profile === undefined) {
+                throw new R2Error(
+                    'No active profile found',
+                    'Unable to disable mods when active profile is not set.'
+                )
+            }
+
+            await dispatch('disableModsFromProfile', {...params, profile});
+        },
+
+        async disableModsFromProfile(
+            {dispatch},
+            params: {
+                mods: ManifestV2[],
+                profile: Profile,
+                onProgress?: (mod: ManifestV2) => void,
+            }
+        ) {
+            const {mods, profile, onProgress} = params;
+            let lastSuccessfulUpdate: ManifestV2[] | undefined;
+
+            try {
+                // Disable mods on disk.
+                for (const mod of mods) {
+                    onProgress && onProgress(mod);
+
+                    if (!mod.isEnabled()) {
+                        continue;
+                    }
+
+                    const err = await ProfileInstallerProvider.instance.disableMod(mod, profile);
+                    if (err instanceof R2Error) {
+                        throw err;
+                    }
+                }
+
+                // Update mod list status to mods.yml and Vuex.
+                const updatedList = await ProfileModList.updateMods(mods, profile, (mod) => mod.disable());
+                if (updatedList instanceof R2Error) {
+                    throw updatedList;
+                } else {
+                    lastSuccessfulUpdate = updatedList;
+                }
+            } finally {
+                if (lastSuccessfulUpdate !== undefined) {
+                    dispatch('updateModList', lastSuccessfulUpdate, {root: true});
+                }
+            }
+
+            // IDK but sounds important.
+            if (lastSuccessfulUpdate !== undefined) {
+                const err = await ConflictManagementProvider.instance.resolveConflicts(
+                    lastSuccessfulUpdate,
+                    profile
+                );
+                if (err instanceof R2Error) {
+                    throw err;
+                }
+            }
         },
     },
 }
