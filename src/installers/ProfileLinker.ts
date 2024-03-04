89fdbd6f1f9514c2aa1c4db055b8c68dcda1ff99
Ethan Green <ethangreen.dev@gmail.com>
2024-03-03 19:37:18 -0500

    remove unused class

--- src/installers/ProfileLinker.ts
+++ src/installers/ProfileLinker.ts
@@ -3,11 +3,6 @@ import ManifestV2 from "../model/ManifestV2";
 import { InstallArgs } from "./PackageInstaller";
 import Game from "../model/game/Game";
 
-export type LinkArgs = {
-    profile: Profile;
-    installDir: string,
-};
-
 export abstract class ProfileLinker {
     abstract perform(profile: Profile, game: Game, gameDir: string): Promise<string[]>;
     // abstract unwind(args: LinkArgs): Promise<void>; <-- TODO
