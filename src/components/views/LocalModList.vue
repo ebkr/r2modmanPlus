c724c02d2e0ab27ec031963de85964c6569cd8d0
Antti Mäki <antti@anttimaki.com>
2024-03-06 17:57:33 +0200

    LocalModList: use Vuex to save mod list to disk after reordering

--- src/components/views/LocalModList.vue
+++ src/components/views/LocalModList.vue
@@ -27,10 +27,8 @@
 
 import { Component, Vue } from 'vue-property-decorator';
 import ManifestV2 from '../../model/ManifestV2';
-import ProfileModList from '../../r2mm/mods/ProfileModList';
 import R2Error from '../../model/errors/R2Error';
 import Profile from '../../model/Profile';
-import ConflictManagementProvider from '../../providers/generic/installing/ConflictManagementProvider';
 import Draggable from 'vuedraggable';
 import AssociatedModsModal from './LocalModList/AssociatedModsModal.vue';
 import DisableModModal from './LocalModList/DisableModModal.vue';
@@ -56,27 +54,18 @@ import SearchAndSort from './LocalModList/SearchAndSort.vue';
         }
 
         set draggableList(newList: ManifestV2[]) {
-            ProfileModList.requestLock(async () => {
-                const result = await ProfileModList.saveModList(this.contextProfile!, newList);
-                if (result instanceof R2Error) {
-                    this.$store.commit('error/handleError', result);
-                    return;
-                }
-                this.updateModListAfterChange(newList);
-            })
+            try {
+                this.$store.dispatch(
+                    'profile/saveModListToDisk',
+                    {mods: newList, profile: this.contextProfile!}
+                );
+            } catch (e) {
+                this.$store.commit('error/handleError', R2Error.fromThrownValue(e));
+            }
         }
 
         get profileName() {
             return this.contextProfile!.getProfileName();
-        }
-
-        async updateModListAfterChange(updatedList: ManifestV2[]) {
-            await this.$store.dispatch('profile/updateModList', updatedList);
-
-            const err = await ConflictManagementProvider.instance.resolveConflicts(updatedList, this.contextProfile!);
-            if (err instanceof R2Error) {
-                this.$store.commit('error/handleError', err);
-            }
         }
 
         async created() {
