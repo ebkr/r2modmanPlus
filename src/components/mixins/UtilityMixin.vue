3e9f018303fe36f06d85a254324f272cbf660d1b
Antti Mäki <antti@anttimaki.com>
2024-03-19 16:21:39 +0200

    Combine "API response to IndexedDB" methods behind a single Vuex action
    Update the exclusion list (if not already done), filter the response
    for excluded packages, and store the rest into IndexedDB in one fell
    swoop.

--- src/components/mixins/UtilityMixin.vue
+++ src/components/mixins/UtilityMixin.vue
@@ -6,7 +6,6 @@ import R2Error from '../../model/errors/R2Error';
 import GameManager from '../../model/game/GameManager';
 import CdnProvider from '../../providers/generic/connection/CdnProvider';
 import ConnectionProvider from '../../providers/generic/connection/ConnectionProvider';
-import * as PackageDb from '../../r2mm/manager/PackageDexieStore';
 
 @Component
 export default class UtilityMixin extends Vue {
@@ -27,10 +26,8 @@ export default class UtilityMixin extends Vue {
             return;
         }
 
-        await this.$store.dispatch('tsMods/updateExclusions');
         const response = await ConnectionProvider.instance.getPackages(GameManager.activeGame);
-        const packages = this.$store.getters['tsMods/filterExcluded'](response.data);
-        await PackageDb.updateFromApiResponse(GameManager.activeGame.internalFolderName, packages);
+        await this.$store.dispatch("tsMods/updatePersistentCache", response.data);
         await this.$store.dispatch("tsMods/updateMods");
         await this.$store.dispatch("profile/tryLoadModListFromDisk");
     }
