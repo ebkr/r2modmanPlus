46ffe027a4a191eb4095957fadaab7d33104f053
Ethan Green <ethangreen.dev@gmail.com>
2024-04-18 13:45:03 -0400

    add: vertigo2, against the storm, lycans, castle story

--- src/r2mm/installing/profile_installers/ModLoaderVariantRecord.ts
+++ src/r2mm/installing/profile_installers/ModLoaderVariantRecord.ts
@@ -168,6 +168,10 @@ const VARIANTS = {
     ContentWarning: MODLOADER_PACKAGES,
     Balatro: MODLOADER_PACKAGES,
     BoplBattle: MODLOADER_PACKAGES,
+    Vertigo2: MODLOADER_PACKAGES,
+    AgainstTheStorm: MODLOADER_PACKAGES,
+    Lycans: MODLOADER_PACKAGES,
+    CastleStory: MODLOADER_PACKAGES,
 };
 // Exported separately from the definition in order to preserve the key names in the type definition.
 // Otherwise this would become [key: string] and we couldn't use the game names for type hinting elsewhere.
