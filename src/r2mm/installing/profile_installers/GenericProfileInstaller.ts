56e1e1616bae8ae09ab094e38d81b3e92ab84a49
xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>
2024-05-20 22:54:01 +0200

    better error msg

--- src/r2mm/installing/profile_installers/GenericProfileInstaller.ts
+++ src/r2mm/installing/profile_installers/GenericProfileInstaller.ts
@@ -211,9 +211,10 @@ export default class GenericProfileInstaller extends ProfileInstallerProvider {
         const fs = FsProvider.instance;
         const bepInExVariant = MOD_LOADER_VARIANTS[activeGame.internalFolderName];
         if (bepInExVariant.find(value => value.packageName.toLowerCase() === mod.getName().toLowerCase())) {
+            let filePath = ""
             try {
                 for (const file of (await fs.readdir(profile.getPathOfProfile()))) {
-                    const filePath = path.join(profile.getPathOfProfile(), file);
+                    filePath = path.join(profile.getPathOfProfile(), file);
                     if ((await fs.lstat(filePath)).isFile()) {
                         if (file.toLowerCase() !== 'mods.yml') {
                             await fs.unlink(filePath);
@@ -221,7 +222,7 @@ export default class GenericProfileInstaller extends ProfileInstallerProvider {
                     }
                 }
             } catch(e) {
-                const name = 'Failed to delete BepInEx file from profile root';
+                const name = `Failed to delete mod loader file (Path: ${filePath}) from profile root`;
                 const solution = 'Is the game still running?';
                 return FileWriteError.fromThrownValue(e, name, solution);
             }
