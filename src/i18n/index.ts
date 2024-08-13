bb3239fb0e8b4302abf76bfe2cfe38c0e52f4df6
Michael Lagardera <michael.lagardera@gmail.com>
2024-08-13 18:59:37 -0400

    Finished Spanish translation. Added esUS to i18n index.ts

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,6 +1,7 @@
 import enUS from './en-us';
 import zhCN from './zh-cn';
+import esUS from './es-us';
 
 export default {
-  'en-us': enUS, 'zh-cn': zhCN,
+  'en-us': enUS, 'zh-cn': zhCN, 'es-us': esUS,
 };
