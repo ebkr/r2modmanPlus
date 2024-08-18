ba9e961cfe6b494fd4e125e433d99a5f461e56f7
Cade Ayres <ca469@kent.ac.uk>
2024-08-18 16:56:27 +0100

    Added `max_old_space_size` argument to environment

--- quasar.conf.js
+++ quasar.conf.js
@@ -52,7 +52,9 @@ module.exports = configure(function(/* ctx */) {
         // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
         build: {
             vueRouterMode: 'hash', // available values: 'hash', 'history'
-
+            env: {
+                NODE_OPTIONS: '--max_old_space_size=4096'
+            },
             win: {
                 publish: {
                     provider: 'github'
