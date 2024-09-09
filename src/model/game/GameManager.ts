6ee38f559b62db589263b8ba2d9d7035c6e1fa63
Antti Mäki <antti@anttimaki.com>
2024-09-09 17:04:58 +0300

    Change Hades 2 cover image to align hame titles vertically

--- src/model/game/GameManager.ts
+++ src/model/game/GameManager.ts
@@ -750,7 +750,7 @@ export default class GameManager {
         new Game("Hades II", "HadesII", "HadesII",
             "Hades II/Ship", ["Hades2.exe"], "",
             "https://thunderstore.io/c/hades-ii/api/v1/package/", EXCLUSIONS,
-            [new StorePlatformMetadata(StorePlatform.STEAM, "1145350")], "HadesII.jpg",
+            [new StorePlatformMetadata(StorePlatform.STEAM, "1145350")], "Hades2.png",
             GameSelectionDisplayMode.VISIBLE, GameInstanceType.GAME, PackageLoader.RETURN_OF_MODDING, ["h2"]),
     ];
 
