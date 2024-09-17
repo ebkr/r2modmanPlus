1fc61a57997d8c7079401d33de729df12afee108
Antti Mäki <antti@anttimaki.com>
2024-09-17 13:24:18 +0300

    Add ManagerUtils.filterModVersionsByDate
    This helper can be used to filter mod versions by start and/or end
    date. If all versions of a mod are filtered out, the whole mod is also
    removed.
    
    The idea is to create a "Time Bubble" feature. User could choose per
    profile the dates between which the online mod list is filtered.
    Intended use case is game updates breaking mods: if user updates the
    game, they can filter out older versions to see only the mods that have
    been updated after the breaking change. If user sticks to the old
    version of the game, they can filter out newer mods that don't work on
    their version, and avoid recommendations to update mods that would just
    break things.

--- src/utils/ManagerUtils.ts
+++ src/utils/ManagerUtils.ts
@@ -41,6 +41,45 @@ export const filterModVersions = (modList: ThunderstoreMod[], dependencyString: 
 }
 
 /**
+ * Filters out versions that are created outside the time bubble defined by the
+ * date arguments. If all versions are filtered out, removes the whole package.
+ *
+ * MUTATES THE LIST IN PLACE. Theoretically there's no upper limit to how large
+ * the modList can be, so avoid making a copy unnecessarily.
+ *
+ * This can be used to improve the changes of creating a mod profile compatible
+ * with a specific version of a game. If a game update breaks all mods, user
+ * can choose to stay on an older game version and filter out newer mods, or to
+ * use the new game version and filter out mods that haven't been updated yet.
+ */
+export const filterModVersionsByDate = (modList: ThunderstoreMod[], startDate?: string, endDate?: string) => {
+    if (startDate && endDate && startDate > endDate) {
+        throw new Error("Argument startDate can't be greater than endDate")
+    }
+    if (!startDate && !endDate) {
+        return;
+    }
+
+    for (let i = modList.length - 1; i >= 0; i--) {
+        const filtered = modList[i].getVersions().filter(
+            (v) => (!startDate || v.getDateCreated() >= startDate) && (!endDate || v.getDateCreated() <= endDate)
+        );
+
+        if (filtered.length) {
+            modList[i].setVersions(filtered);
+            modList[i].setDateUpdated(filtered[filtered.length - 1].getDateCreated());
+        } else {
+            modList.splice(i, 1);
+        }
+    }
+
+    // TODO(?): this takes around 20ms with current LC mod list. It's only needed when
+    // the default ordering is used so it could be done conditionally, but that would
+    // require refactoring the sorting mode out of the OnlineModView component.
+    modList.sort(ThunderstoreMod.defaultOrderComparer);
+}
+
+/**
  * Return default game selection needed to skip the game selection screen.
  */
 export const getDefaults = (settings: ManagerSettings) => {
