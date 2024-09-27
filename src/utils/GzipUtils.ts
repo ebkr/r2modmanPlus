8aeaaad93c598a10f990b66e72050a36d182a4ce
Antti Mäki <antti@anttimaki.com>
2024-09-27 11:21:19 +0300

    Use fflate to decompress package list chunks
    While r2modman used Node's zlib as intended, TSMM used a library that
    happened to be present as a 2nd degree dependency. While it worked, the
    library had poor performance, taking ~3s to decompress each chunk,
    while zlib and fflate takes next to no time at all.
    
    Using DecompressionStream instead was considered, but based on a single
    google result it seems that would require updating the TypeScript
    version, which is a side quest for another time.
    
    It was also considered sniffing the app version and using fflate only
    on TSMM, so it wouldn't need to be added as a dependency on r2modman.
    In the end gut feeling decided that would be unnecessarily complex and
    prone to break due to future changes.

--- src/utils/GzipUtils.ts
+++ src/utils/GzipUtils.ts
@@ -1,13 +1,18 @@
-import * as zlib from 'zlib';
+// Use third party library as Node's zlib is not available on TSMM.
+import { AsyncGunzip } from "fflate";
 
 export function decompressArrayBuffer(compressed: ArrayBuffer, encoding = 'utf-8'): Promise<string> {
     return new Promise((resolve, reject) => {
-        zlib.gunzip(compressed, (err, result) => {
+        const gunzip = new AsyncGunzip((err, result) => {
             if (err) {
                 return reject(err);
             }
 
-            return resolve(result.toString(encoding));
+            const decoder = new TextDecoder(encoding);
+            const decodedString = decoder.decode(result);
+            return resolve(decodedString);
         });
+
+        gunzip.push(new Uint8Array(compressed), true);
     });
 }
