73c3ba891a99245133026b03cdda34ac86b0e630
Ross Brunton <bruntonross@protonmail.com>
2024-10-02 23:58:46 +0100

    Option to use Flatpaks under Linux
    This adds a configuration setting "linuxUseFlatpak" which controls
    whether Steam should be launched as a Flatpak or not. By default this
    is detected automatically based on the path, but an option in the
    settings can override the behaviour.
    
    If this option is enabled, we first check if we are able to read from
    our config directory. If not, we display an error and ask the user to
    give us permission. If we do, then we launch Steam through flatpak
    rather than the binary directly.

--- src/pages/Manager.vue
+++ src/pages/Manager.vue
@@ -370,6 +370,10 @@ import ModalCard from '../components/ModalCard.vue';
 			this.settings.setFunkyMode(value);
 		}
 
+		setLinuxUseFlatpak(value: boolean) {
+			this.settings.setLinuxUseFlatpak(value);
+		}
+
 		async exportProfile() {
 			if (!this.localModList.length) {
 				const err = new R2Error(
@@ -578,6 +582,9 @@ import ModalCard from '../components/ModalCard.vue';
                     break;
                 case "ToggleFunkyMode":
                     this.setFunkyMode(!this.settings.getContext().global.funkyModeEnabled);
+                    break;
+                case "ToggleLinuxUseFlatpak":
+                    this.setLinuxUseFlatpak(!this.settings.getContext().global.linuxUseFlatpak);
                     break;
                 case "SwitchTheme":
                     this.toggleDarkTheme();
