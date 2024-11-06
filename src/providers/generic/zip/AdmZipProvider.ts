802a4a74c89cb3f8927de8dfe8e7828dc0c591be
753.network <PrimaryInfinity@gmail.com>
2024-11-06 10:22:49 -0500

    Update AdmZipProvider.ts
    Ensure zips extract to intended paths

--- src/providers/generic/zip/AdmZipProvider.ts
+++ src/providers/generic/zip/AdmZipProvider.ts
@@ -1,18 +1,14 @@
 import ZipProvider from './ZipProvider';
-import AdmZip, { IZipEntry } from 'adm-zip';
+import AdmZip from 'adm-zip';
 import * as path from 'path';
 import ZipBuilder from './ZipBuilder';
 import ZipEntryInterface from './ZipEntryInterface';
-import FileUtils from '../../../utils/FileUtils';
-import FsProvider from '../file/FsProvider';
 
 export default class AdmZipProvider extends ZipProvider {
 
     async extractAllTo(zip: string | Buffer, outputFolder: string): Promise<void> {
         const adm = new AdmZip(zip);
-        for (let entry of adm.getEntries()) {
-            await this.sanitizedExtraction(entry, outputFolder);
-        }
+        adm.extractAllTo(outputFolder, true);
     }
 
     async readFile(zip: string | Buffer, file: string): Promise<Buffer | null> {
@@ -27,16 +23,11 @@ export default class AdmZipProvider extends ZipProvider {
 
     async extractEntryTo(zip: string | Buffer, target: string, outputPath: string): Promise<void> {
         const adm = new AdmZip(zip);
-        return this.sanitizedExtraction(adm.getEntry(target)!, outputPath);
-    }
-
-    private async sanitizedExtraction(entry: IZipEntry, outputPath: string): Promise<void> {
-        const sanitizedTargetName = entry.entryName.split('\\').join('/');
-        await FileUtils.ensureDirectory(path.dirname(path.join(outputPath, sanitizedTargetName)));
-        if (entry.isDirectory)
-            await FileUtils.ensureDirectory(path.join(outputPath, sanitizedTargetName));
-        else
-            await FsProvider.instance.writeFile(path.join(outputPath, sanitizedTargetName), entry.getData());
+        if(!path.posix.normalize(path.join(outputPath, target)).startsWith(outputPath))
+        {
+            throw Error("Entry " + target + " would extract outside of expected folder");
+        }
+        adm.extractEntryTo(target, outputPath, true, true);
     }
 
     zipBuilder(): ZipBuilder {
