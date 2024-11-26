fb3123cf2c002b0bf8107d80871be73a6df65759
Vili Manninen <vili.manninen@riskidev.fi>
2024-11-26 17:06:25 +0200

    Update Gladio Mori SteamFolderName and storeIdentifier

--- src/model/game/GameManager.ts
+++ src/model/game/GameManager.ts
@@ -659,9 +659,9 @@ export default class GameManager {
             GameSelectionDisplayMode.VISIBLE, GameInstanceType.GAME, PackageLoader.BEPINEX, ["act"]),
 
         new Game("Gladio Mori", "GladioMori", "GladioMori",
-            "Gladio Mori Demo", ["Gladio Mori.exe"], "Gladio Mori_Data",
+            "Gladio Mori", ["Gladio Mori.exe"], "Gladio Mori_Data",
             "https://thunderstore.io/c/gladio-mori/api/v1/package-listing-index/", EXCLUSIONS,
-            [new StorePlatformMetadata(StorePlatform.STEAM, "2908480")], "GladioMori.png",
+            [new StorePlatformMetadata(StorePlatform.STEAM, "2689120")], "GladioMori.png",
             GameSelectionDisplayMode.VISIBLE, GameInstanceType.GAME, PackageLoader.BEPINEX, ["gm"]),
 
         new Game("Slipstream: Rogue Space", "SlipstreamRogueSpace", "SlipstreamRogueSpace",
