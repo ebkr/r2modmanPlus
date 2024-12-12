f8147e73f351101ec2c61cb85e50b16cfe359694
Antti Mäki <antti@anttimaki.com>
2024-12-12 13:43:00 +0200

    Remove callbacks from LocalModInstallerProvider methods

--- src/r2mm/installing/LocalModInstaller.ts
+++ src/r2mm/installing/LocalModInstaller.ts
@@ -42,59 +42,33 @@ export default class LocalModInstaller extends LocalModInstallerProvider {
         await FsProvider.instance.writeFile(manifestPath, JSON.stringify(manifest));
     }
 
-    public async extractToCacheWithManifestData(profile: ImmutableProfile, zipFile: string, manifest: ManifestV2, callback: (success: boolean, error: R2Error | null) => void) {
+    public async extractToCacheWithManifestData(profile: ImmutableProfile, zipFile: string, manifest: ManifestV2) {
         const cacheDirectory: string = await this.initialiseCacheDirectory(manifest);
-
-        try {
-            await ZipExtract.extractOnly(zipFile, cacheDirectory);
-        } catch (e) {
-            callback(false, R2Error.fromThrownValue(e, `Extracting ${zipFile} failed`));
-            return Promise.resolve();
-        }
-
-        try {
-            await this.writeManifestToCache(cacheDirectory, manifest);
-        } catch (e) {
-            callback(false, R2Error.fromThrownValue(e));
-            return Promise.resolve();
-        }
-
+        await ZipExtract.extractOnly(zipFile, cacheDirectory);
+        await this.writeManifestToCache(cacheDirectory, manifest);
         await ProfileInstallerProvider.instance.uninstallMod(manifest, profile);
-        const profileInstallResult = await ProfileInstallerProvider.instance.installMod(manifest, profile);
-        if (profileInstallResult instanceof R2Error) {
-            callback(false, profileInstallResult);
-            return Promise.resolve();
-        }
-        const modListInstallResult = await ProfileModList.addMod(manifest, profile);
-        if (modListInstallResult instanceof R2Error) {
-            callback(false, modListInstallResult);
-            return Promise.resolve();
-        }
-        callback(true, null);
-        return Promise.resolve();
+        throwForR2Error(await ProfileInstallerProvider.instance.installMod(manifest, profile));
+        throwForR2Error(await ProfileModList.addMod(manifest, profile));
     }
 
-    public async placeFileInCache(profile: ImmutableProfile, file: string, manifest: ManifestV2, callback: (success: boolean, error: (R2Error | null)) => void) {
+    public async placeFileInCache(profile: ImmutableProfile, file: string, manifest: ManifestV2) {
         try {
             const cacheDirectory: string = await this.initialiseCacheDirectory(manifest);
             const fileSafe = file.split("\\").join("/");
             await FsProvider.instance.copyFile(fileSafe, path.join(cacheDirectory, path.basename(fileSafe)));
             await this.writeManifestToCache(cacheDirectory, manifest);
-            await ProfileInstallerProvider.instance.uninstallMod(manifest, profile);
-            const profileInstallResult = await ProfileInstallerProvider.instance.installMod(manifest, profile);
-            if (profileInstallResult instanceof R2Error) {
-                callback(false, profileInstallResult);
-                return Promise.resolve();
-            }
-            const modListInstallResult = await ProfileModList.addMod(manifest, profile);
-            if (modListInstallResult instanceof R2Error) {
-                callback(false, modListInstallResult);
-                return Promise.resolve();
-            }
-            callback(true, null);
-            return Promise.resolve();
         } catch (e) {
-            callback(false, new R2Error("Error moving file to cache", (e as Error).message, null));
+            throw R2Error.fromThrownValue(e, "Error moving file to cache");
         }
+
+        await ProfileInstallerProvider.instance.uninstallMod(manifest, profile);
+        throwForR2Error(await ProfileInstallerProvider.instance.installMod(manifest, profile));
+        throwForR2Error(await ProfileModList.addMod(manifest, profile));
+    }
+}
+
+function throwForR2Error(maybeError: unknown) {
+    if (maybeError instanceof R2Error) {
+        throw maybeError;
     }
 }
