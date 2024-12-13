10af1db1dc5cd3b1976b9748e5e8aa2bed08001a
Vili Manninen <104134747+VilppeRiskidev@users.noreply.github.com>
2024-12-13 09:18:15 +0200

    More immutable profile (#1579)
    * Use ImmutableProfile in ConflictManagementProvider
    * Use ImmutableProfile in LocalModInstallerProvider

--- src/providers/generic/installing/ConflictManagementProvider.ts
+++ src/providers/generic/installing/ConflictManagementProvider.ts
@@ -1,7 +1,7 @@
 import ProviderUtils from '../../generic/ProviderUtils';
 import ManifestV2 from '../../../model/ManifestV2';
 import R2Error from '../../../model/errors/R2Error';
-import Profile, { ImmutableProfile } from '../../../model/Profile';
+import { ImmutableProfile } from '../../../model/Profile';
 
 export default abstract class ConflictManagementProvider {
 
@@ -18,11 +18,11 @@ export default abstract class ConflictManagementProvider {
         return ConflictManagementProvider.provider();
     }
 
-    public abstract resolveConflicts(mods: ManifestV2[], profile: Profile): Promise<R2Error | void>;
+    public abstract resolveConflicts(mods: ManifestV2[], profile: ImmutableProfile): Promise<R2Error | void>;
 
     public abstract overrideInstalledState(mod: ManifestV2, profile: ImmutableProfile): Promise<R2Error | void>;
 
-    public abstract isFileActive(mod: ManifestV2, profile: Profile|ImmutableProfile, file: string): Promise<boolean>;
+    public abstract isFileActive(mod: ManifestV2, profile: ImmutableProfile, file: string): Promise<boolean>;
 
 }
 
