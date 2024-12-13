d39bee27efe991a069b0ddebf3daa4b0aae3bb80
Vili Manninen <vili.manninen@riskidev.fi>
2024-12-13 14:16:31 +0200

    Add a getter activeDownloadCount

--- src/store/modules/DownloadModule.ts
+++ src/store/modules/DownloadModule.ts
@@ -1,3 +1,6 @@
+import { GetterTree } from "vuex";
+import { State as RootState } from "../../store";
+
 export interface DownloadProgress {
     assignId: number;
     initialMods: string[];
@@ -24,6 +27,15 @@ export const DownloadModule = {
         assignId: 0,
         downloadObject: null, // TODO: Check if the last element of allVersions can be used instead of this
     }),
+
+    getters: <GetterTree<State, RootState>>{
+        activeDownloadCount(state) {
+            const active = state.allVersions.filter(
+                dl => !dl[1].failed && dl[1].progress < 100
+            );
+            return active.length;
+        },
+    },
 
     mutations: {
         setDownloadObject(state: State, downloadObject: DownloadProgress) {
