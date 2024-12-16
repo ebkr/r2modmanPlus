0c9d999c63ce404fee36ea62f52c59915036082d
VilppeRiskidev <vili.manninen@riskidev.fi>
2024-12-16 14:54:28 +0200

    Add game: Subterror

--- src/r2mm/installing/profile_installers/ModLoaderVariantRecord.ts
+++ src/r2mm/installing/profile_installers/ModLoaderVariantRecord.ts
@@ -210,6 +210,7 @@ const VARIANTS = {
     STRAFTAT: MODLOADER_PACKAGES,
     ATLYSS: MODLOADER_PACKAGES,
     PeaksOfYore: MODLOADER_PACKAGES,
+    Subterror: MODLOADER_PACKAGES,
 };
 // Exported separately from the definition in order to preserve the key names in the type definition.
 // Otherwise this would become [key: string] and we couldn't use the game names for type hinting elsewhere.
