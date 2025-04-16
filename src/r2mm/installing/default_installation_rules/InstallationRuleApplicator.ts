dc8c0eb209009eedfdd1a45e331418fb40723701
Antti Mäki <antti@anttimaki.com>
2025-04-16 14:39:32 +0300

    Fix InstallationRules to work with ecosystem data
    - Map tracking method strings from ecosystem data to the types used in
      the mod manager
    - Include relativeFileExclusions in the code now that they are
      available in the ecosystem data
    
    Mediate differences between ecosystem data

--- src/r2mm/installing/default_installation_rules/InstallationRuleApplicator.ts
+++ src/r2mm/installing/default_installation_rules/InstallationRuleApplicator.ts
@@ -1,13 +1,13 @@
-import InstallationRules, { CoreRuleType } from '../InstallationRules';
+import InstallationRules, { normalizeRuleSubtype } from '../InstallationRules';
 import { EcosystemSchema } from '../../../model/schema/ThunderstoreSchema';
 
 export default class InstallationRuleApplicator {
 
     public static apply() {
-        InstallationRules.RULES = EcosystemSchema.supportedGames
-            .map((x) => <CoreRuleType>{
-                gameName: x.r2modman!.internalFolderName,
-                rules: x.r2modman!.installRules,
-            });
+        InstallationRules.RULES = EcosystemSchema.supportedGames.map((x) => ({
+            gameName: x.r2modman.internalFolderName,
+            rules: x.r2modman.installRules.map(normalizeRuleSubtype),
+            relativeFileExclusions: x.r2modman.relativeFileExclusions,
+        }));
     }
 }
