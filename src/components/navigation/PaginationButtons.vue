e5b14aa9a94e574d3ece16e6d9ce33a125bab8a1
Cade Ayres <ca469@kent.ac.uk>
2025-06-05 11:57:22 +0100

    Ported PaginationButtons.vue to Composition API

--- src/components/navigation/PaginationButtons.vue
+++ src/components/navigation/PaginationButtons.vue
@@ -1,39 +1,30 @@
-<script lang="ts">
-import { Vue, Component, Prop, Watch } from 'vue-property-decorator';
-import { truncatePagination } from "../../utils/Pagination";
+<script lang="ts" setup>
+import { truncatePagination } from '../../utils/Pagination';
+import { computed } from 'vue';
 
-@Component({})
-export default class PaginationButtons extends Vue {
-    @Prop({ required: true })
-    private currentPage!: number;
-
-    @Prop({ required: true })
-    private pageCount!: number;
-
-    @Prop({ required: true })
-    private contextSize!: number;
-
-    @Prop({ required: true })
-    private onClick!: (pageIndex: number) => void;
-
-    @Watch("currentPage")
-    @Watch("pageCount")
-    @Watch("contextSize")
-    visibleButtons() {
-        return truncatePagination({
-            currentPage: this.currentPage,
-            pageCount: this.pageCount,
-            contextSize: this.contextSize,
-        });
-    }
+type PaginationButtonsProps = {
+    currentPage: number;
+    pageCount: number;
+    contextSize: number;
+    onClick: (pageIndex: number) => void;
 }
+
+const props = defineProps<PaginationButtonsProps>();
+
+const visibleButtons = computed(() => {
+    return truncatePagination({
+        currentPage: props.currentPage,
+        pageCount: props.pageCount,
+        contextSize: props.contextSize,
+    });
+});
 </script>
 
 <template>
 <nav class="pagination">
     <ul class="pagination-list">
         <li
-            v-for="button in visibleButtons()"
+            v-for="button in visibleButtons"
             :key="`pagination-${button.index}`"
         >
             <a
