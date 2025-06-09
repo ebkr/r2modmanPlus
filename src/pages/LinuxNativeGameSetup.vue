5462182ef981f283b70090617ae767cbbbd7f140
Cade Ayres <ca469@kent.ac.uk>
2025-06-09 16:14:05 +0100

    Ported LinuxNativeGameSetup.vue to Composition API

--- src/pages/LinuxNativeGameSetup.vue
+++ src/pages/LinuxNativeGameSetup.vue
@@ -1,63 +1,60 @@
 <template>
 	<div>
-		<hero :title="`Getting started on ${platformName}`" subtitle="Let's configure the game properly" heroType="warning" />
+		<Hero :title="`Getting started on ${platformName}`" subtitle="Let's configure the game properly" heroType="warning" />
 		<br/>
 		<div class="container">
 			To be able to launch {{ activeGame }} on Linux, you must first setup your Steam launch options correctly.<br/>
 			This needs to be done because of how the BepInEx injection works on Unix systems.<br/>
 			<br/>
 			Please copy and paste the following to your {{ activeGame }} launch options:<br/>
-			<code ref="launchargs">{{ launchArgs }} %command%</code>
+			<code ref="copyableArgs">{{ launchArgs }} %command%</code>
 			<br/>
 			<br/>
-			<a ref="copy" class="button margin-right margin-right--half-width" @click="copy">Copy to clipboard</a>
+			<a ref="copyAction" class="button margin-right margin-right--half-width" @click="copy">Copy to clipboard</a>
 			<a class="button is-info" @click="acknowledge">Continue</a>
 		</div>
 	</div>
 </template>
 
-<script lang='ts'>
+<script lang='ts' setup>
 import PathResolver from '../r2mm/manager/PathResolver';
-import { Component, Vue } from 'vue-property-decorator';
 import { Hero } from '../components/all';
 import * as path from 'path';
+import { computed, getCurrentInstance, onMounted, ref } from 'vue';
+import { getStore } from '../providers/generic/store/StoreProvider';
+import { State } from '../store';
+import VueRouter from 'vue-router';
 
-@Component({
-    components: {
-        'hero': Hero
-    }
+const store = getStore<State>();
+let router!: VueRouter;
+
+onMounted(() => {
+    router = getCurrentInstance()!.proxy.$router;
 })
-export default class LinuxFirstTimeSetup extends Vue {
 
-	data() {
-		return {
-			activeGame: this.$store.state.activeGame.displayName,
-			launchArgs: `"${path.join(PathResolver.MOD_ROOT, process.platform === 'darwin' ? 'macos_proxy' : 'linux_wrapper.sh')}"`
-		}
-	}
+const copyableArgs = ref<HTMLInputElement>();
+const copyAction = ref<HTMLElement>();
 
-    get platformName(): string {
-        return process.platform === 'darwin' ? 'macOS' : process.platform
+const activeGame = computed(() => store.state.activeGame.displayName);
+const launchArgs = computed(() => `"${path.join(PathResolver.MOD_ROOT, process.platform === 'darwin' ? 'macos_proxy' : 'linux_wrapper.sh')}"`);
+const platformName = computed<string>(() => process.platform === 'darwin' ? 'macOS' : process.platform);
+
+function copy(){
+    let range = document.createRange();
+    range.selectNode(copyableArgs.value as Node);
+    const selection = window.getSelection();
+    if(selection !== null) {
+        selection.removeAllRanges();
+        selection.addRange(range);
     }
+    document.execCommand("copy");
+    (copyAction.value as Element).innerHTML = "Copied!";
+    setTimeout(() => {
+        (copyAction.value as Element).innerHTML = "Copy to clipboard";
+    }, 2000);
+}
 
-	private copy(){
-		let range = document.createRange();
-		range.selectNode(this.$refs.launchargs as Node);
-		const selection = window.getSelection();
-		if(selection !== null) {
-			selection.removeAllRanges();
-			selection.addRange(range);
-		}
-		document.execCommand("copy");
-		(this.$refs.copy as Element).innerHTML = "Copied!";
-		setTimeout(() => {
-			(this.$refs.copy as Element).innerHTML = "Copy to clipboard";
-		}, 2000);
-	}
-
-	private async acknowledge(){
-		this.$router.push({path: "/profiles"});
-	}
-
+async function acknowledge(){
+    router.push({path: "/profiles"});
 }
 </script>
