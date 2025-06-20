d52aee5d2b944f42567ee9987862f8ec92443017
Cade Ayres <ca469@kent.ac.uk>
2025-06-20 08:49:10 +0100

    Updated boot files to support Vue 3

--- src/boot/i18n.ts
+++ src/boot/i18n.ts
@@ -1,18 +1,17 @@
-import Vue from 'vue';
-import VueI18n from 'vue-i18n';
+import { createI18n } from 'vue-i18n';
 import messages from '../i18n';
+// You'll need to create the src/i18n/index.js/.ts file too
 
-Vue.use(VueI18n);
+const i18n = createI18n({
+    locale: 'en-US',
+    globalInjection: true,
+    messages
+})
 
-const i18n = new VueI18n({
-  locale: 'en-us',
-  fallbackLocale: 'en-us',
-  messages,
-});
+// @ts-ignore
+export default ({ app }) => {
+    // Tell app to use the I18n instance
+    app.use(i18n)
+}
 
-export default ({ app }: any) => {
-  // Set i18n instance on app
-  app.i18n = i18n;
-};
-
-export { i18n };
+export { i18n }
