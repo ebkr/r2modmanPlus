ff5df8746c3df20344f937ab053e340a406da6f8
Cade Ayres <ca469@kent.ac.uk>
2025-06-20 08:48:55 +0100

    Converted src-electron files to ESM and added electron-env.d.ts

---
+++ src-electron/electron-env.d.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    QUASAR_PUBLIC_FOLDER: string;
+    QUASAR_ELECTRON_PRELOAD_FOLDER: string;
+    QUASAR_ELECTRON_PRELOAD_EXTENSION: string;
+    APP_URL: string;
+  }
+}
