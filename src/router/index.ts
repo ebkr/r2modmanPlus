0c9885f456d5935ddab26937389936d847a24a72
Cade Ayres <ca469@kent.ac.uk>
2025-06-24 12:33:39 +0100

    Upgraded store and router. Deferenced boot files.
    - Boot files are currently causing errors on startup. Needs investigating.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,21 @@
 import { createRouter, createWebHashHistory, RouteLocationNormalized, RouteMeta } from 'vue-router';
 import routes from './routes';
 
-export default function (/* { store, ssrContext } */) {
-    const createHistory = createWebHashHistory;
-    const Router = createRouter({
-        scrollBehavior: () => ({ left: 0, top: 0 }),
-        routes,
-        linkActiveClass: 'is-active',
+const createHistory = createWebHashHistory;
+const Router = createRouter({
+    scrollBehavior: () => ({ left: 0, top: 0 }),
+    routes,
+    linkActiveClass: 'is-active',
 
-        // Leave this as is and make changes in the quasar.config file instead!
-        // quasar.config file -> build -> vueRouterMode
-        // quasar.config file -> build -> publicPath
-        history: createHistory(process.env.VUE_ROUTER_BASE),
-    });
+    // Leave this as is and make changes in the quasar.config file instead!
+    // quasar.config file -> build -> vueRouterMode
+    // quasar.config file -> build -> publicPath
+    history: createHistory(process.env.VUE_ROUTER_BASE)
+});
 
-    Router.afterEach((to: RouteLocationNormalized, ignored: RouteLocationNormalized) => {
-        const meta: RouteMeta = to.meta;
-        document.title = meta.title as string;
-    });
-}
+Router.afterEach((to: RouteLocationNormalized, ignored: RouteLocationNormalized) => {
+    const meta: RouteMeta = to.meta;
+    document.title = meta.title as string;
+});
+
+export default Router;
