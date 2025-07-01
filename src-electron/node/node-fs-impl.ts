b1812013382011ed4a11781a2c7f98a59ec1ad0e
Cade Ayres <ca469@kent.ac.uk>
2025-07-01 13:40:06 +0100

    List of profiles are shown correctly

--- src-electron/node/node-fs-impl.ts
+++ src-electron/node/node-fs-impl.ts
@@ -1,50 +1,57 @@
 import { BrowserWindow, ipcMain } from 'electron';
-import fs from 'fs';
+import fs, { StatsBase } from 'fs';
+import * as url from 'node:url';
 
 export function hookFsIpc(browserWindow: BrowserWindow) {
     ipcMain.on('node:fs:writeFile', (event, identifier, path, content) => {
-        try {
-            fs.writeFileSync(path, content);
-            browserWindow.webContents.send(`node:fs:writeFile:${identifier}`);
-        } catch (e) {
-            browserWindow.webContents.send(`node:fs:writeFile:${identifier}`, e);
-        }
+        fs.promises.writeFile(path, content)
+            .then(() => browserWindow.webContents.send(`node:fs:writeFile:${identifier}`))
+            .catch(e => browserWindow.webContents.send(`node:fs:writeFile:${identifier}`, e));
     });
 
-    ipcMain.on('node:fs:readFile', (event, identifier, path, content) => {
-        try {
-            const result = fs.readFileSync(path, {
-                encoding: 'utf8',
-            });
-            browserWindow.webContents.send(`node:fs:readFile:${identifier}`, result);
-        } catch (e) {
-            browserWindow.webContents.send(`node:fs:readFile:${identifier}`, e);
-        }
+    ipcMain.on('node:fs:readFile', (event, identifier, path) => {
+        fs.promises.readFile(path, {
+            encoding: 'utf8'
+        })
+            .then(result => browserWindow.webContents.send(`node:fs:readFile:${identifier}`, result))
+            .catch(e => browserWindow.webContents.send(`node:fs:readFile:${identifier}`, e))
     });
 
-    ipcMain.on('node:fs:exists',  async (event, identifier, path) => {
-        const result = await fs.promises.access(path, fs.constants.F_OK)
-            .then(() => true)
-            .catch(() => false);
-        console.log("node:fs:exists", path, result);
-        browserWindow.webContents.send(`node:fs:exists:${identifier}`, result);
+    ipcMain.on('node:fs:exists',  (event, identifier, path) => {
+        fs.promises.access(path, fs.constants.F_OK)
+            .then(() => browserWindow.webContents.send(`node:fs:exists:${identifier}`, true))
+            .catch(() => browserWindow.webContents.send(`node:fs:exists:${identifier}`, false));
     });
 
-    ipcMain.on('node:fs:mkdirs',  async (event, {identifier, path}) => {
-        try {
-            await fs.promises.mkdir(path, { recursive: true });
-            browserWindow.webContents.send(`node:fs:mkdirs:${identifier}`);
-        } catch (e) {
-            browserWindow.webContents.send(`node:fs:mkdirs:${identifier}`, e);
-        }
+    ipcMain.on('node:fs:mkdirs',  async (event, identifier, path) => {
+        fs.promises.mkdir(path, { recursive: true })
+            .then(() => browserWindow.webContents.send(`node:fs:mkdirs:${identifier}`))
+            .catch(e => browserWindow.webContents.send(`node:fs:mkdirs:${identifier}`, e));
     });
 
-    ipcMain.on('node:fs:readdir',  async (event, {identifier, path}) => {
-        try {
-            const result = await fs.promises.readdir(path);
-            browserWindow.webContents.send(`node:fs:readdir:${identifier}`, result);
-        } catch (e) {
-            browserWindow.webContents.send(`node:fs:readdir:${identifier}`, e);
-        }
+    ipcMain.on('node:fs:readdir',  (event, identifier, path) => {
+        fs.promises.readdir(path)
+            .then(files => {
+                console.log("Dir for", path, "=", files)
+                return files;
+            })
+            .then(result => browserWindow.webContents.send(`node:fs:readdir:${identifier}`, result))
+            .catch(e => browserWindow.webContents.send(`node:fs:readdir:${identifier}`, e));
     });
+
+    ipcMain.on('node:fs:stat',  (event, identifier, path) => {
+        fs.promises.stat(path)
+            .then(result => browserWindow.webContents.send(`node:fs:stat:${identifier}`, generateInlineStat(result)))
+            .catch(e => browserWindow.webContents.send(`node:fs:stat:${identifier}`, e));
+    });
+}
+
+function generateInlineStat<T extends fs.StatsBase<number>>(statLike: fs.Stats): T {
+    const unpackedStatLike = {
+        ...statLike
+    } as fs.StatsBase<number>;
+    return {
+        ...unpackedStatLike,
+        isDirectory: statLike.isDirectory()
+    } as unknown as T;
 }
