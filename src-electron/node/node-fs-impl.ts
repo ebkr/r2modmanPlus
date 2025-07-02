e14840d3ac425cc8cb9822e999b02f70f08b3a1a
Cade Ayres <ca469@kent.ac.uk>
2025-07-02 11:54:15 +0100

    Implemented full fs and path libraries

--- src-electron/node/node-fs-impl.ts
+++ src-electron/node/node-fs-impl.ts
@@ -32,7 +32,6 @@ export function hookFsIpc(browserWindow: BrowserWindow) {
     ipcMain.on('node:fs:readdir',  (event, identifier, path) => {
         fs.promises.readdir(path)
             .then(files => {
-                console.log("Dir for", path, "=", files)
                 return files;
             })
             .then(result => browserWindow.webContents.send(`node:fs:readdir:${identifier}`, result))
@@ -43,6 +42,68 @@ export function hookFsIpc(browserWindow: BrowserWindow) {
         fs.promises.stat(path)
             .then(result => browserWindow.webContents.send(`node:fs:stat:${identifier}`, generateInlineStat(result)))
             .catch(e => browserWindow.webContents.send(`node:fs:stat:${identifier}`, e));
+    });
+
+    ipcMain.on('node:fs:lstat',  (event, identifier, path) => {
+        fs.promises.stat(path)
+            .then(result => browserWindow.webContents.send(`node:fs:lstat:${identifier}`, generateInlineStat(result)))
+            .catch(e => browserWindow.webContents.send(`node:fs:lstat:${identifier}`, e));
+    });
+
+    ipcMain.on('node:fs:rmdir',  (event, identifier, path) => {
+        fs.promises.rmdir(path)
+            .then(() => browserWindow.webContents.send(`node:fs:rmdir:${identifier}`))
+            .catch(e => browserWindow.webContents.send(`node:fs:rmdir:${identifier}`, e))
+    });
+
+    ipcMain.on('node:fs:unlink', (event, identifier, path) => {
+        fs.promises.unlink(path)
+            .then(() => browserWindow.webContents.send(`node:fs:unlink:${identifier}`))
+            .catch(e => browserWindow.webContents.send(`node:fs:unlink:${identifier}`, e))
+    });
+
+    ipcMain.on('node:fs:realpath', (event, identifier, path) => {
+        fs.promises.realpath(path)
+            .then(() => browserWindow.webContents.send(`node:fs:realpath:${identifier}`))
+            .catch(e => browserWindow.webContents.send(`node:fs:realpath:${identifier}`, e))
+    });
+
+    ipcMain.on('node:fs:rename', (event, identifier, path, newPath) => {
+        fs.promises.rename(path, newPath)
+            .then(() => browserWindow.webContents.send(`node:fs:rename:${identifier}`))
+            .catch(e => browserWindow.webContents.send(`node:fs:rename:${identifier}`, e))
+    });
+
+    ipcMain.on('node:fs:chmod', (event, identifier, path, mode) => {
+        fs.promises.chmod(path, mode)
+            .then(() => browserWindow.webContents.send(`node:fs:chmod:${identifier}`))
+            .catch(e => browserWindow.webContents.send(`node:fs:chmod:${identifier}`, e))
+    });
+
+    ipcMain.on('node:fs:copyFile', (event, identifier, from, to) => {
+        fs.promises.copyFile(from, to)
+            .then(() => browserWindow.webContents.send(`node:fs:copyFile:${identifier}`))
+            .catch(e => browserWindow.webContents.send(`node:fs:copyFile:${identifier}`, e))
+    });
+
+    ipcMain.on('node:fs:copyFolder', (event, identifier, from, to) => {
+        fs.promises.copyFile(from, to)
+            .then(() => browserWindow.webContents.send(`node:fs:copyFolder:${identifier}`))
+            .catch(e => browserWindow.webContents.send(`node:fs:copyFolder:${identifier}`, e))
+    });
+
+    ipcMain.on('node:fs:base64FromZip', (event, identifier, path) => {
+        fs.promises.readFile(path, {
+            encoding: 'base64'
+        })
+            .then(result => browserWindow.webContents.send(`node:fs:base64FromZip:${identifier}`, result))
+            .catch(e => browserWindow.webContents.send(`node:fs:base64FromZip:${identifier}`, e))
+    });
+
+    ipcMain.on('node:fs:setModifiedTime', (event, identifier, path, time) => {
+        fs.promises.utimes(path, time, time)
+            .then(() => browserWindow.webContents.send(`node:fs:setModifiedTime:${identifier}`))
+            .catch(e => browserWindow.webContents.send(`node:fs:setModifiedTime:${identifier}`, e))
     });
 }
 
