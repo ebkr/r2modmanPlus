70d93e90fa4c6b7a2e8e267d66d9675a58637a79
cade <ca469@kent.ac.uk>
2025-07-04 16:00:47 +0100

    Added OS library IPC implementation

--- src-electron/ipc/init-ipc.ts
+++ src-electron/ipc/init-ipc.ts
@@ -4,11 +4,13 @@ import { hookChildProcessIpc } from './node-child-process-impl';
 import { hookFsIpc } from 'app/src-electron/ipc/node-fs-impl';
 import { hookZipIpc } from 'app/src-electron/ipc/zip-hook';
 import { hookElectronIpc } from 'app/src-electron/ipc/electron-hook';
+import {hookOsIpc} from "app/src-electron/ipc/node-os-impl";
 
 export function hookIpc(browserWindow: BrowserWindow) {
     hookPathIpc(browserWindow);
     hookChildProcessIpc(browserWindow);
     hookFsIpc(browserWindow);
+    hookOsIpc(browserWindow);
     hookZipIpc(browserWindow);
     hookElectronIpc(browserWindow);
 }
