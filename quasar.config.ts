61acd8b10291e5199ba0b2af82f2d9dd6202488d
Cade Ayres <ca469@kent.ac.uk>
2025-07-04 13:04:28 +0100

    Fixed tooltips and re-added i18n boot file

--- quasar.config.ts
+++ quasar.config.ts
@@ -13,7 +13,7 @@ export default defineConfig((ctx) => {
         // --> boot files are part of "main.js"
         // https://v2.quasar.dev/quasar-cli-vite/boot-files
         boot: [
-            // 'i18n',
+            'i18n',
             // 'axios',
             'floating-vue'
         ],
