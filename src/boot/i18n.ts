7d430243ed18acd7cad9d4fac879878e154772c6
Cade Ayres <ca469@kent.ac.uk>
2025-07-04 13:04:28 +0100

    Fixed tooltips and re-added i18n boot file

--- src/boot/i18n.ts
+++ src/boot/i18n.ts
@@ -1,5 +1,6 @@
 import { createI18n } from 'vue-i18n';
 import messages from '../i18n';
+import { defineBoot } from '#q-app/wrappers';
 // You'll need to create the src/i18n/index.js/.ts file too
 
 const i18n = createI18n({
@@ -9,9 +10,9 @@ const i18n = createI18n({
 })
 
 // @ts-ignore
-export default ({ app }) => {
+export default defineBoot(async ({ app }) => {
     // Tell app to use the I18n instance
     app.use(i18n)
-}
+});
 
 export { i18n }
