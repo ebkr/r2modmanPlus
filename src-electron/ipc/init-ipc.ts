db172c1f0bb0ebe3d9680c4d469773dbf6766f28
Cade Ayres <ca469@kent.ac.uk>
2025-07-04 14:22:13 +0100

    Converted InteractionProviderImpl to use IPC

--- src-electron/ipc/init-ipc.ts
+++ src-electron/ipc/init-ipc.ts
@@ -3,7 +3,7 @@ import { hookPathIpc } from './node-path-impl';
 import { hookChildProcessIpc } from './node-child-process-impl';
 import { hookFsIpc } from 'app/src-electron/ipc/node-fs-impl';
 import { hookZipIpc } from 'app/src-electron/ipc/zip-hook';
-import { hookElectronIpc } from 'app/src-electron/ipc/electron-shell-hook';
+import { hookElectronIpc } from 'app/src-electron/ipc/electron-hook';
 
 export function hookIpc(browserWindow: BrowserWindow) {
     hookPathIpc(browserWindow);
--- src-electron/preload/electron-preload.ts
+++ src-electron/preload/electron-preload.ts
@@ -1,4 +1,4 @@
-import { ipcRenderer } from 'electron';
+import { ipcRenderer, OpenDialogOptions } from 'electron';
 
 export function openExternal(url: string) {
     ipcRenderer.send('electron:shell:openExternal', url);
@@ -10,4 +10,31 @@ export function selectFile(url: string) {
 
 export function openPath(url: string) {
     ipcRenderer.send('electron:shell:openPath', url);
+}
+export async function selectFolderDialog(options: any) {
+    return new Promise(resolve => {
+        const fileOpts = options as unknown as OpenDialogOptions;
+        fileOpts.properties = ['openDirectory', 'showHiddenFiles'];
+
+        ipcRenderer.once('receive-open-dialog', (_, args) => {
+            resolve(args.filePaths);
+        });
+        ipcRenderer.send('show-open-dialog', fileOpts);
+    });
+}
+
+export async function selectFileDialog(options: any) {
+    return new Promise(resolve => {
+        const fileOpts = options as unknown as OpenDialogOptions;
+        fileOpts.properties = ['openFile', 'showHiddenFiles'];
+
+        ipcRenderer.once('receive-open-dialog', (_, args) => {
+            resolve(args.filePaths);
+        });
+        ipcRenderer.send('show-open-dialog', fileOpts);
+    });
+}
+
+export function copyToClipboard(value: string) {
+    ipcRenderer.sendSync('electron:clipboard:copyText', value);
 }
