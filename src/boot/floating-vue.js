2a77e8bd7ace1d6369c25cf7e0e4b5b11ff45848
Cade Ayres <ca469@kent.ac.uk>
2025-07-04 13:04:28 +0100

    Fixed tooltips and re-added i18n boot file

--- src/boot/floating-vue.js
+++ src/boot/floating-vue.js
@@ -1,10 +1,7 @@
-// import FloatingVue, { VTooltip } from 'floating-vue';
-// import 'floating-vue/dist/style.css';
-//
-// // more info on params: https://quasar.dev/quasar-cli/boot-files
-// export default async ({ Vue }) => {
-//   // more info on config: https://floating-vue.starpad.dev/guide/config.html
-//   FloatingVue.options.distance = 12;
-//
-//   Vue.directive('tooltip', VTooltip);
-// }
+import FloatingVue, { VTooltip } from 'floating-vue';
+import 'floating-vue/dist/style.css';
+import { defineBoot } from '#q-app/wrappers'
+
+export default defineBoot(async ({ app }) => {
+    app.use(FloatingVue);
+})
