44de4a8b6c91266aa2bfc52ccf225254f900a4d9
cade <ca469@kent.ac.uk>
2025-07-23 14:07:11 +0100

    ConfigRawEditor can now save/load file text

--- src/components/config-components/v2/ConfigRawEditor.vue
+++ src/components/config-components/v2/ConfigRawEditor.vue
@@ -1,15 +1,34 @@
 <template>
-    <div class="notification margin-right">
-        <QuillEditor
-            :modelValue="fileText"
-            @update:modelValue="quillEditorValueUpdated"/>
+    <div>
+        <div class="sticky-top sticky-top--buttons margin-right">
+            <button class="button is-info margin-right margin-right--half-width" @click="save">Save</button>
+            <button class="button is-danger" @click="cancel">Cancel</button>
+        </div>
+        <div class="notification margin-right">
+            <QuillEditor
+                :modelValue="fileText"
+                @update:modelValue="quillEditorValueUpdated"/>
+        </div>
     </div>
 </template>
 <script setup lang="ts">
 import QuillEditor from "../../QuillEditor.vue";
 import {ref} from "vue";
+import FsProvider from "src/providers/generic/file/FsProvider";
+
+export type ConfigRawEditorProps = {
+    filePath: string;
+}
+
+const props = defineProps<ConfigRawEditorProps>();
+const emits = defineEmits<{
+    (e: 'changed'): void;
+}>();
 
 const fileText = ref<string>("");
+
+FsProvider.instance.readFile(props.filePath)
+    .then(buffer => fileText.value = buffer.toString());
 
 function updateFreeText(text: string) {
     fileText.value = text;
@@ -18,5 +37,14 @@ function updateFreeText(text: string) {
 function quillEditorValueUpdated(value: string) {
     fileText.value = value;
     updateFreeText(value);
+}
+
+async function save() {
+    await FsProvider.instance.writeFile(props.filePath, fileText.value)
+    emits('changed');
+}
+
+function cancel() {
+    emits('changed');
 }
 </script>
