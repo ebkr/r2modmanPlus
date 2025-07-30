398799f3f08fff49c93f731bea7e5e66a8a9f478
Cade Ayres <ca469@kent.ac.uk>
2025-07-30 14:03:38 +0100

    Ported Game installer tests

---
+++ test/vitest/tests/unit/Installers/Game/Install.NASB.spec.ts
@@ -0,0 +1,60 @@
+import InMemoryFsProvider from '../../../../../jest/__tests__/stubs/providers/InMemory.FsProvider';
+import * as path from 'path';
+import ManifestV2 from '../../../../../../src/model/ManifestV2';
+import Profile from '../../../../../../src/model/Profile';
+import ProfileInstallerProvider from '../../../../../../src/providers/ror2/installing/ProfileInstallerProvider';
+import {
+    createManifest,
+    installLogicBeforeEach,
+    setupFolderStructureTestFiles,
+    testStateTrackedFileStructure,
+    testSubdirTrackedFileStructure,
+    testUntrackedFileStructure
+} from '../../../../../jest/__utils__/InstallLogicUtils';
+import {describe, beforeAll, afterAll, test} from 'vitest';
+
+let pkg: ManifestV2;
+
+describe('NASB Install Logic', () => {
+
+    beforeAll(async () => {
+        await installLogicBeforeEach("NASB");
+        pkg = createManifest('test_mod', 'author');
+        await setupFolderStructureTestFiles(pkg);
+
+        await ProfileInstallerProvider.instance.installMod(pkg, Profile.getActiveProfile().asImmutableProfile());
+
+        // Correct folder name casing conversion should happen within the ProfileInstaller.
+        // Tests would get fairly heavily hard-coded if recorded here.
+        InMemoryFsProvider.setMatchMode("CASE_INSENSITIVE");
+    });
+
+    afterAll(() => {
+        InMemoryFsProvider.clear();
+        InMemoryFsProvider.setMatchMode("CASE_SENSITIVE");
+    })
+
+    test('SUBDIR', async () => {
+        await testSubdirTrackedFileStructure(pkg, [
+            [path.join("BIE", "Plugins"), "BepInEx"],
+            [path.join("BIE", "Monomod"), "BepInEx"],
+            [path.join("BIE", "Patchers"), "BepInEx"],
+            [path.join("BIE", "Core"), "BepInEx"],
+            [path.join("BIE", "GameSpecific", "NASB", "Voicepacks"), "BepInEx"],
+            [path.join("BIE", "GameSpecific", "NASB", "Skins"), "BepInEx"],
+        ]);
+    });
+
+    test('NONE', async () => {
+        await testUntrackedFileStructure([
+            [path.join("BIE", "Config"), "BepInEx"],
+        ]);
+    });
+
+    test('STATE', async () => {
+        await testStateTrackedFileStructure(pkg, [
+            [path.join('BIE', 'GameSpecific', 'NASB', 'CustomSongs'), path.join("BepInEx")],
+            [path.join('BIE', 'GameSpecific', 'NASB', 'Movesets'), path.join('BepInEx')]
+        ]);
+    });
+});
