107d1c5522ea604fba26d5e17be5d0e9fb61bc9f
Cade Ayres <ca469@kent.ac.uk>
2025-07-31 09:30:10 +0100

    Added test coverage for buildConfigurationFileFromPath
    - Short refactor to reduce dependency on old ConfigFile

---
+++ test/jest/__tests__/utils/utils.ConfigUtils.ts.spec.ts
@@ -0,0 +1,184 @@
+import FsProvider from '../../../../src/providers/generic/file/FsProvider';
+import InMemoryFsProvider from '../stubs/providers/InMemory.FsProvider';
+import { buildConfigurationFileFromPath, CommentLine, ConfigurationEntry } from '../../../../src/utils/ConfigUtils';
+import path from 'path';
+
+describe('buildConfigurationFileFromPath', () => {
+
+    beforeAll(() => {
+        const fsProvider = new InMemoryFsProvider();
+        FsProvider.provide(() => fsProvider);
+    });
+
+    beforeEach(() => {
+        InMemoryFsProvider.clear();
+    });
+
+    test('Standard KeyValue with Section', async () => {
+        const fileContent = [
+            '[Section Name]',
+            'FirstKey = Identifier',
+            '',
+            'SecondKey = 345',
+            'ThirdKey = #AABBCC'
+        ].join('\r\n');
+
+        const pathToFileFolder = 'config';
+        const fileName = 'standard_keyvalue_with_section.cfg';
+        const filePath = path.join(pathToFileFolder, fileName);
+        await FsProvider.instance.mkdirs(pathToFileFolder);
+        await FsProvider.instance.writeFile(filePath, fileContent);
+
+        const configurationFile = await buildConfigurationFileFromPath(filePath);
+
+        expect(configurationFile.filename).toStrictEqual(fileName);
+        expect(configurationFile.path).toStrictEqual(filePath);
+
+        const sectionNames = configurationFile.sections.map((section) => section.sectionName);
+
+        expect(sectionNames).toStrictEqual(['Section Name']);
+
+        const section = configurationFile.sections[0];
+        expect(section.entries).toMatchObject([
+            {
+                entryName: 'FirstKey',
+                value: 'Identifier',
+                cachedValue: 'Identifier',
+                displayType: 'input',
+                commentLines: []
+            },
+            {
+                entryName: 'SecondKey',
+                value: '345',
+                cachedValue: '345',
+                displayType: 'input',
+                commentLines: []
+            },
+            {
+                entryName: 'ThirdKey',
+                value: '#AABBCC',
+                cachedValue: '#AABBCC',
+                displayType: 'input',
+                commentLines: []
+            }
+        ] as ConfigurationEntry[]);
+    });
+
+    test('Entry comments', async () => {
+        const fileContent = [
+            '[Section Name]',
+            '## This is a description comment',
+            '# This is a metadata comment',
+            '',
+            '# A further metadata comment',
+            'Key = Value',
+        ].join('\r\n');
+
+        const pathToFileFolder = 'config';
+        const fileName = 'entry_comments.cfg';
+        const filePath = path.join(pathToFileFolder, fileName);
+        await FsProvider.instance.mkdirs(pathToFileFolder);
+        await FsProvider.instance.writeFile(filePath, fileContent);
+
+        const configurationFile = await buildConfigurationFileFromPath(filePath);
+
+        expect(configurationFile.filename).toStrictEqual(fileName);
+        expect(configurationFile.path).toStrictEqual(filePath);
+
+        const sectionNames = configurationFile.sections.map((section) => section.sectionName);
+
+        expect(sectionNames).toStrictEqual(['Section Name']);
+
+        const section = configurationFile.sections[0];
+        expect(section.entries).toMatchObject([
+            {
+                entryName: 'Key',
+                value: 'Value',
+                cachedValue: 'Value',
+                displayType: 'input',
+                commentLines: [
+                    {
+                        rawValue: '## This is a description comment',
+                        displayValue: 'This is a description comment',
+                        isDescription: true
+                    },
+                    {
+                        rawValue: '# This is a metadata comment',
+                        displayValue: 'This is a metadata comment',
+                        isDescription: false
+                    },
+                    {
+                        rawValue: '# A further metadata comment',
+                        displayValue: 'A further metadata comment',
+                        isDescription: false
+                    }
+                ]
+            }
+        ] as ConfigurationEntry[]);
+    });
+
+    describe("Additional input type identification", () => {
+
+        test("Single select", async () => {
+            const fileContent = [
+                '[Section Name]',
+                '# Acceptable values: a, b',
+                'FirstKey = Identifier',
+            ].join('\r\n');
+
+            const pathToFileFolder = 'config';
+            const fileName = 'standard_keyvalue_with_section.cfg';
+            const filePath = path.join(pathToFileFolder, fileName);
+            await FsProvider.instance.mkdirs(pathToFileFolder);
+            await FsProvider.instance.writeFile(filePath, fileContent);
+
+            const configurationFile = await buildConfigurationFileFromPath(filePath);
+            const entry = configurationFile.sections[0].entries[0];
+
+            expect(entry.displayType).toStrictEqual('single-select');
+        });
+
+        test("Multi select", async () => {
+            const fileContent = [
+                '[Section Name]',
+                '# Multiple values can be set at the same time by separating them with commas',
+                '# Acceptable values: a, b',
+                'FirstKey = Identifier',
+            ].join('\r\n');
+
+            const pathToFileFolder = 'config';
+            const fileName = 'standard_keyvalue_with_section.cfg';
+            const filePath = path.join(pathToFileFolder, fileName);
+            await FsProvider.instance.mkdirs(pathToFileFolder);
+            await FsProvider.instance.writeFile(filePath, fileContent);
+
+            const configurationFile = await buildConfigurationFileFromPath(filePath);
+            const entry = configurationFile.sections[0].entries[0];
+
+            expect(entry.displayType).toStrictEqual('multi-select');
+        });
+
+        test("Boolean", async () => {
+            const fileContent = [
+                '[Section Name]',
+                '# Setting type: Boolean',
+                '# Acceptable values: true, false',
+                'FirstKey = Identifier',
+            ].join('\r\n');
+
+            const pathToFileFolder = 'config';
+            const fileName = 'standard_keyvalue_with_section.cfg';
+            const filePath = path.join(pathToFileFolder, fileName);
+            await FsProvider.instance.mkdirs(pathToFileFolder);
+            await FsProvider.instance.writeFile(filePath, fileContent);
+
+            const configurationFile = await buildConfigurationFileFromPath(filePath);
+            const entry = configurationFile.sections[0].entries[0];
+
+            expect(entry.displayType).toStrictEqual('boolean');
+        });
+
+    })
+
+});
+
