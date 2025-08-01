700bd84f14f6ee17496c725c3cb2a75c34255887
cade <ca469@kent.ac.uk>
2025-08-01 17:07:38 +0100

    Split translations into clearer modules

---
+++ src/i18n/en/pages/GameSelectionTranslation.ts
@@ -0,0 +1,39 @@
+import {GameSelectionMessageFormat} from "../../base/pages/GameSelectionMessageFormat";
+
+export const GameSelectionTranslation: GameSelectionMessageFormat = {
+    platformModal: {
+        header: "Which store manages your game?",
+        selectAction: "Select platform",
+    },
+    pageTitle: {
+        title: {
+            game: 'Game selection',
+            server: 'Server selection',
+        },
+        subtitle: {
+            game: 'Which game are you managing your mods for?',
+            server: 'Which dedicated server are you managing your mods for?',
+        }
+    },
+    migrationNotice: {
+        requiresUpdate: 'An update to the manager has occurred and needs to do background work.',
+        actionsDisabled: 'The options to select a game are disabled until the work has completed.',
+    },
+    tabs: {
+        game: 'Game',
+        server: 'Server'
+    },
+    actions: {
+        select: {
+            game: 'Select game',
+            server: 'Select server'
+        },
+        setAsDefault: 'Set as default'
+    },
+    filter: {
+        placeholder: {
+            game: 'Search for a game',
+            server: 'Search for a server',
+        }
+    }
+}
---
+++ src/i18n/fr/platforms/PlatformTranslation.ts
@@ -0,0 +1,11 @@
+import {PlatformMessageFormat} from "../../base/platforms/PlatformMessageFormat";
+
+export const PlatformTranslation: PlatformMessageFormat = {
+    STEAM: 'Steam',
+    STEAM_DIRECT: 'Steam',
+    EPIC_GAMES_STORE: 'Epic Games Store',
+    OCULUS_STORE: 'Oculus / Meta Magasin',
+    ORIGIN: 'Origin / EA App',
+    XBOX_GAME_PASS: 'Xbox Game Pass',
+    OTHER: 'Autre'
+}
