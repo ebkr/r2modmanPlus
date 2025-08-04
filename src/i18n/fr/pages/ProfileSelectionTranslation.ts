952508b32cbeec92e17d75116b02ed43a71394d2
cade <ca469@kent.ac.uk>
2025-08-04 14:40:59 +0100

    Added i18n support for CreateProfileModal and DeleteProfileModal

--- src/i18n/fr/pages/ProfileSelectionTranslation.ts
+++ src/i18n/fr/pages/ProfileSelectionTranslation.ts
@@ -18,8 +18,27 @@ export const ProfileSelectionTranslation: ProfileSelectionMessageFormat = {
         updateProfileList: 'Erreur lors de la mise à jour de la liste de profils'
     },
     createProfileModal: {
+        title: 'Créer un profil',
+        description: 'Ce profil stockera ses propres mods indépendamment des autres profils.',
+        tagStates: {
+            required: 'Vous devez entrer un nom de profil',
+            valid: '"{profileName}" est un nom de profil valide',
+            error: '"{profileName}" est déjà utilisé ou contient des caractères non valides'
+        },
+        actions: {
+            create: 'Créer'
+        }
     },
     deleteProfileModal: {
+        title: 'Supprimer un profil',
+        content: {
+            resultingAction: 'Cela supprimera tous les mods et leurs fichiers de configuration installés dans ce profil.',
+            preventAction: 'Si c\'est une erreur, cliquez sur la zone assombrie ou sur la croix en haut à droite.',
+            confirmation: 'Êtes-vous sûr de vouloir supprimer ce profil ?'
+        },
+        action: {
+            delete: 'Supprimer le profil'
+        }
     },
     renameProfileModal: {
     },
