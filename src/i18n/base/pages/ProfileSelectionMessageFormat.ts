f2dbadaf70cc7edfaf4ba7b776ee9458902057ef
cade <ca469@kent.ac.uk>
2025-08-06 13:47:01 +0100

    Added English translations for ImportProfileModal

--- src/i18n/base/pages/ProfileSelectionMessageFormat.ts
+++ src/i18n/base/pages/ProfileSelectionMessageFormat.ts
@@ -51,5 +51,97 @@ export type ProfileSelectionMessageFormat = {
         },
     },
     importProfileModal: {
+        states: {
+            fileCodeSelection: {
+                title: string;
+                actions: {
+                    fromCode: string;
+                    fromFile: string;
+                }
+            },
+            importCode: {
+                title: string;
+                enterCodePlaceholder: string;
+                actions: {
+                    proceed: string;
+                    loading: string;
+                },
+                tagStates: {
+                    invalid: string;
+                }
+            },
+            fromFile: {
+                title: string;
+                content: string;
+            },
+            refresh: {
+                title: string;
+                content: {
+                    description: string;
+                    waitingForModDownloads: string;
+                    refreshStatus: {
+                        checkingForUpdates: string;
+                        loadingLatestModList: string;
+                        pruneCache: string;
+                        processingModList: string;
+                        almostDone: string;
+                        resettingCache: string;
+                    };
+                };
+            },
+            reviewImport: {
+                title: string;
+                content: {
+                    notFoundDisclaimer: string;
+                    ensureCorrectProfile: string;
+                    packagesWillBeInstalled: string;
+                },
+                actions: {
+                    acknowledgement: string;
+                    proceed: string;
+                }
+            },
+            willImportOrUpdate: {
+                title: string;
+                actions: {
+                    newProfile: string;
+                    existingProfile: string;
+                }
+            },
+            addProfile: {
+                title: string;
+                content: {
+                    create: {
+                        description: string;
+                    },
+                    update: {
+                        contentsWillBeOverwritten: string;
+                        selectProfile: string;
+                    }
+                };
+                tagStates: {
+                    required: string;
+                    valid: string;
+                    error: string;
+                },
+                actions: {
+                    create: string;
+                    update: string;
+                }
+            },
+            importInProgress: {
+                title: {
+                    downloadingMods: string;
+                    cleaningUp: string;
+                    applyChanges: string,
+                    copyingModsToProfile: string;
+                    copyingConfigsToProfile: string;
+                },
+                content: {
+                    waitMessage: string;
+                    doNotClose: string;
+                }
+            }
+        }
     }
 }
--- src/store/modules/TsModsModule.ts
+++ src/store/modules/TsModsModule.ts
@@ -29,6 +29,7 @@ export interface State {
     modsLastUpdated?: Date | undefined;
     thunderstoreModListUpdateError: Error|undefined;
     thunderstoreModListUpdateStatus: string;
+    thunderstoreModListUpdateProgress: number;
 }
 
 type ProgressCallback = (progress: number) => void;
@@ -73,7 +74,8 @@ export const TsModsModule = {
         /*** Error shown on UI after mod list refresh fails */
         thunderstoreModListUpdateError: undefined,
         /*** Status shown on UI during mod list refresh */
-        thunderstoreModListUpdateStatus: ''
+        thunderstoreModListUpdateStatus: '',
+        thunderstoreModListUpdateProgress: 0,
     }),
 
     getters: <GetterTree<State, RootState>>{
@@ -174,6 +176,9 @@ export const TsModsModule = {
         setThunderstoreModListUpdateStatus(state, status: string) {
             state.thunderstoreModListUpdateStatus = status;
         },
+        setThunderstoreModListUpdateProgress(state, progress: number) {
+            state.thunderstoreModListUpdateProgress = progress;
+        },
         startThunderstoreModListUpdate(state) {
             state.isThunderstoreModListUpdateInProgress = true;
             state.thunderstoreModListUpdateError = undefined;
@@ -215,7 +220,7 @@ export const TsModsModule = {
             commit('startThunderstoreModListUpdate');
 
             try {
-                commit('setThunderstoreModListUpdateStatus', 'Checking for mod list updates from Thunderstore...');
+                commit('setThunderstoreModListUpdateStatus', 'checkingForUpdates');
                 const packageListIndex = await dispatch('fetchPackageListIndex');
 
                 // If the package list is up to date, only update the timestamp. Otherwise,
@@ -227,10 +232,10 @@ export const TsModsModule = {
                         'fetchAndCachePackageListChunks',
                         {
                             packageListIndex,
-                            progressCallback: (progress: number) => commit(
-                                'setThunderstoreModListUpdateStatus',
-                                `Loading latest mod list from Thunderstore: ${progress}%`
-                            ),
+                            progressCallback: (progress: number) => {
+                                commit('setThunderstoreModListUpdateStatus', 'loadingLatestModList');
+                                commit('setThunderstoreModListUpdateProgress', progress);
+                            },
                         },
                     );
                 }
@@ -244,9 +249,9 @@ export const TsModsModule = {
                 if (packageListIndex.isLatest && state.mods.length > 0) {
                     await dispatch('updateModsLastUpdated');
                 } else {
-                    commit('setThunderstoreModListUpdateStatus', 'Processing the mod list...');
+                    commit('setThunderstoreModListUpdateStatus', 'processingModList');
                     await dispatch('updateMods');
-                    commit('setThunderstoreModListUpdateStatus', 'Almost done...');
+                    commit('setThunderstoreModListUpdateStatus', 'almostDone');
                     await dispatch('profile/tryLoadModListFromDisk', null, {root: true});
                 }
             } catch (e) {
@@ -372,7 +377,7 @@ export const TsModsModule = {
             const community = rootState.activeGame.internalFolderName;
 
             try {
-                commit('setThunderstoreModListUpdateStatus', 'Resetting mod list cache...');
+                commit('setThunderstoreModListUpdateStatus', 'resettingCache');
                 await PackageDb.resetCommunity(community);
                 commit('setModsLastUpdated', undefined);
             } finally {
