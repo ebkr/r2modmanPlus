edb6b51c301d425e35ceadf252f753b3ed3a215a
Cade Ayres <ca469@kent.ac.uk>
2025-08-11 10:20:39 +0100

    Added i18n support to UpdateAllInstalledModsModal

--- src/components/views/UpdateAllInstalledModsModal.vue
+++ src/components/views/UpdateAllInstalledModsModal.vue
@@ -6,8 +6,10 @@ import ThunderstoreCombo from '../../model/ThunderstoreCombo';
 import { getStore } from '../../providers/generic/store/StoreProvider';
 import { State } from '../../store';
 import { InstallMode } from '../../utils/DependencyUtils';
+import { useI18n } from 'vue-i18n';
 
 const store = getStore<State>();
+const { t } = useI18n();
 
 const isOpen = computed(() => store.state.modals.isUpdateAllModsModalOpen);
 const modsWithUpdates = computed(() => store.getters['profile/modsWithUpdates']);
@@ -32,33 +34,41 @@ async function updateAllToLatestVersion() {
 <template>
     <ModalCard id="update-all-installed-mods-modal" :is-active="isOpen" :can-close="true" v-if="modsWithUpdates.length === 0" @close-modal="closeModal()">
         <template v-slot:header>
-            <h2 class='modal-title'>No mods to update</h2>
+            <h2 class='modal-title'>
+                {{ t('translations.pages.manager.modals.updateAllInstalledMods.noModsToUpdate.title') }}
+            </h2>
         </template>
         <template v-slot:body>
-            <p>Either all installed mods are up to date, or there are no installed mods.</p>
+            <p>{{ t('translations.pages.manager.modals.updateAllInstalledMods.noModsToUpdate.content')}}</p>
         </template>
         <template v-slot:footer>
-            <button class="button is-info" @click="closeModal()">Close</button>
+            <button class="button is-info" @click="closeModal()">
+                {{ t('translations.pages.manager.modals.updateAllInstalledMods.noModsToUpdate.close') }}
+            </button>
         </template>
     </ModalCard>
     <ModalCard id="update-all-installed-mods-modal" :is-active="isOpen" :can-close="true" v-else @close-modal="closeModal()">
         <template v-slot:header>
-            <h2 class='modal-title'>Update all installed mods</h2>
+            <h2 class='modal-title'>
+                {{ t('translations.pages.manager.modals.updateAllInstalledMods.hasModsToUpdate.title') }}
+            </h2>
         </template>
         <template v-slot:body>
-            <p>All installed mods will be updated to their latest versions.</p>
-            <p>Any missing dependencies will be installed.</p>
-            <p>The following mods will be downloaded and installed:</p>
+            <p>{{ t('translations.pages.manager.modals.updateAllInstalledMods.hasModsToUpdate.content.willBeUpdated') }}</p>
+            <p>{{ t('translations.pages.manager.modals.updateAllInstalledMods.hasModsToUpdate.content.missingDependenciesInstalled') }}</p>
+            <p>{{ t('translations.pages.manager.modals.updateAllInstalledMods.hasModsToUpdate.content.whatWillHappen') }}</p>
             <br/>
             <ul class="list">
                 <li class="list-item" v-for='(mod, index) in modsWithUpdates'
                     :key='`to-update-${index}-${mod.getFullName()}`'>
-                    {{mod.getName()}} will be updated to: {{mod.getLatestVersion()}}
+                    {{ t('translations.pages.manager.modals.updateAllInstalledMods.hasModsToUpdate.content.modUpdatedTo', { modName: mod.getName(), version: mod.getLatestVersion() }) }}
                 </li>
             </ul>
         </template>
         <template v-slot:footer>
-            <button class="button is-info" @click="updateAllToLatestVersion()">Update all</button>
+            <button class="button is-info" @click="updateAllToLatestVersion()">
+                {{ t('translations.pages.manager.modals.updateAllInstalledMods.hasModsToUpdate.updateAll') }}
+            </button>
         </template>
     </ModalCard>
 </template>
