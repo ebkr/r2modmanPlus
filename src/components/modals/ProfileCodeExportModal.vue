6b89c8bc9d161c2f283b4b01676111557c1f3988
cade <ca469@kent.ac.uk>
2025-08-11 08:31:36 +0100

    Added i18n support for code export modal

--- src/components/modals/ProfileCodeExportModal.vue
+++ src/components/modals/ProfileCodeExportModal.vue
@@ -1,13 +1,27 @@
 <script lang="ts" setup>
 import ModalCard from '../ModalCard.vue';
-import { computed } from 'vue';
+import {computed, ref} from 'vue';
 import { getStore } from '../../providers/generic/store/StoreProvider';
 import { State } from '../../store';
+import {useI18n} from "vue-i18n";
+import InteractionProvider from "../../providers/ror2/system/InteractionProvider";
 
 const store = getStore<State>();
-const isOpen = computed(() => store.state.modals.isProfileCodeExportModalOpen);
+const { t } = useI18n();
 
+const isOpen = computed(() => store.state.modals.isProfileCodeExportModalOpen);
 const exportCode = computed(() => store.state.profileExport.exportCode);
+
+const isCopied = ref<boolean>(false);
+const exportCodeText = computed(() => isCopied.value ? 'Copied to clipboard' : exportCode.value);
+
+function copyCode() {
+    InteractionProvider.instance.copyToClipboard(exportCode.value);
+    isCopied.value = true;
+    setTimeout(() => {
+        isCopied.value = false;
+    }, 1000);
+}
 
 function closeModal() {
     store.commit("closeProfileCodeExportModal");
@@ -17,16 +31,24 @@ function closeModal() {
 <template>
     <ModalCard id="profile-exported-modal" :is-active="isOpen" @close-modal="closeModal" :can-close="true">
         <template v-slot:header>
-            <h2 class='modal-title'>Profile exported</h2>
+            <h2 class='modal-title'>
+                {{ t('translations.pages.manager.modals.codeExport.title') }}
+            </h2>
         </template>
         <template v-slot:body>
-            <p>Your code: <strong>{{exportCode}}</strong> has been copied to your clipboard. Just give it to a
-                friend!
+            <p>
+                {{ t('translations.pages.manager.modals.codeExport.description') }}
             </p>
+            <code class="code">
+                <code>{{ exportCodeText }}</code>
+                <a href="#" @click="copyCode">
+                    <i class="fas fa-copy"></i>
+                </a>
+            </code>
         </template>
         <template v-slot:footer>
             <button class="button is-info" @click="closeModal">
-                Done
+                {{ t('translations.pages.manager.modals.codeExport.done') }}
             </button>
         </template>
     </ModalCard>
