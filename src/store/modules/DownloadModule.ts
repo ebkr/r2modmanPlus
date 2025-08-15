2da3e6f2fada66dc5787abf4a68489a6f23a7649
Cade Ayres <ca469@kent.ac.uk>
2025-08-15 10:35:05 +0100

    Addressed more PR comments:
    - Nits / comment changes
    
    - Restore old functionality to setIconPath
    
    - Changed Profiles.vue onMounted to be sequential rather than lambda style promises.

--- src/store/modules/DownloadModule.ts
+++ src/store/modules/DownloadModule.ts
@@ -274,7 +274,6 @@ export const DownloadModule = {
             }
         },
         addDownload(state: State, download: DownloadProgress) {
-            // @ts-ignore
             state.allDownloads = [...state.allDownloads, download];
         },
         setDone(state: State, downloadId: number) {
