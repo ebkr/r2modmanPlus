9c6af678b5a0c3a2151142cf1d675d96f04fc91f
Cade Ayres <ca469@kent.ac.uk>
2025-08-18 15:58:44 +0100

    Removed redundant catch block

--- src/providers/node/child_process/ChildProcessImplementation.ts
+++ src/providers/node/child_process/ChildProcessImplementation.ts
@@ -5,16 +5,13 @@ let execIdentifier = 0;
 export const NodeChildProcessImplementation: NodeChildProcessProvider = {
     execSync: path => window.node.child_process.execSync(path),
     exec: async (path, options, callback) => {
+        // Error is passed directly into resolve to keep compatibility with callback implementations.
+        // Therefore no need to use a `.catch` block
         window.node.child_process.exec(path, options)
             .then(err => {
                 if (callback) {
                     callback(err as unknown as Error);
                 }
-            })
-            .catch(err => {
-                if (callback) {
-                    callback(err as Error);
-                }
-            })
+            });
     }
 }
