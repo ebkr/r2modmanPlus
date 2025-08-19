f4e3420425420470455e864da7b5441810a43e50
Cade Ayres <ca469@kent.ac.uk>
2025-08-19 15:41:13 +0100

    Added provider to get public asset path

--- src/providers/node/buffer/BufferImplementation.ts
+++ src/providers/node/buffer/BufferImplementation.ts
@@ -1,7 +1,5 @@
 import { NodeBufferProvider } from './buffer';
 
-let execIdentifier = 0;
-
 export const NodeBufferImplementation: NodeBufferProvider = {
     from: (data, encoding) => window.node.buffer.from(data, encoding),
 }
