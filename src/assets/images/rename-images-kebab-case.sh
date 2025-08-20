cfec9e48dee5affd60849e165b0088a811f4da58
Vili Manninen <vili.manninen@riskidev.fi>
2025-08-20 15:45:12 +0300

    Add a image name format validator script and delete unneeded scripts

--- src/assets/images/rename-images-kebab-case.sh
+++ src/assets/images/rename-images-kebab-case.sh
@@ -1,6 +1,37 @@
 #!/bin/bash
 
-source "$(dirname "$0")/kebab-case-utils.sh"
+convert_to_kebab_case() {
+    local base="$1"
+    local kebab=""
+
+    # Check if base name is all caps (only uppercase letters and numbers)
+    if [[ "$base" =~ ^[[:upper:][:digit:]]+$ ]]; then
+        kebab=$(echo "$base" | tr '[:upper:]' '[:lower:]')
+    # Check if the name is already in kebab-case
+    elif [[ "$base" =~ ^[[:lower:][:digit:]]+(-[[:lower:][:digit:]]+)*$ ]]; then
+        kebab="$base" # Name is already in kebab-case
+    else
+        kebab=$(echo "$base" | sed 's/[_-]/ /g') # Replace underscores and other separators with spaces
+
+        # Split numbers and letters ("20MinutesTillDawn" -> "20 Minutes Till Dawn" and "Hades2" -> "Hades 2")
+        # Skip for all-caps strings (like "H3VR")
+        if [[ ! "$base" =~ ^[[:upper:][:digit:]]+$ ]]; then
+            kebab=$(echo "$kebab" | sed 's/\([0-9]\)\([A-Za-z]\)/\1 \2/g; s/\([A-Za-z]\)\([0-9]\)/\1 \2/g')
+        fi
+        
+        kebab=$(echo "$kebab" | sed 's/\([a-z]\)\([A-Z]\)/\1 \2/g') # Split uppercase after lowercase ("AmongUs" -> "Among Us")
+        
+        kebab=$(echo "$kebab" | sed 's/\([A-Z]\)\([A-Z][a-z]\)/\1 \2/g') # Uppercases letters followed by an uppercase letter should stay as a group ("TCGCardShopSimulator" -> "TCG Card Shop Simulator")
+        
+        kebab=$(echo "$kebab" | sed 's/[^a-zA-Z0-9]\+/-/g') # Replace non-alphanumeric (spaces, etc.) with hyphens
+        
+        kebab=$(echo "$kebab" | tr '[:upper:]' '[:lower:]') # Convert to lowercase
+        
+        kebab=$(echo "$kebab" | sed 's/^-//; s/-$//; s/--*/-/g') # Remove extra hyphens
+    fi
+
+    echo "$kebab"
+}
 
 target_dir="game_selection"
 
