f8d7ea53f61f4038ba609ecf9db5bcd2d0d4d4cd
Cade Ayres <ca469@kent.ac.uk>
2025-08-22 10:39:21 +0100

    Addressed PR comments

--- src/providers/node/app/AppWindowImplementation.ts
+++ src/providers/node/app/AppWindowImplementation.ts
@@ -1,7 +1,5 @@
 import { AppWindowProvider } from './app_window';
 
-let execIdentifier = 0;
-
 export const AppWindowImplementation: AppWindowProvider = {
     getPlatform: () => window.app.getPlatform(),
 }
