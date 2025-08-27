ec2ce02b583a75ba0df0da3277d313dc249e12a5
Gregorio Litenstein <g.litenstein@gmail.com>
2025-08-27 18:09:12 -0400

    Remove superfluous promises
    Signed-off-by: Gregorio Litenstein <g.litenstein@gmail.com>

--- src/providers/generic/file/NodeFs.ts
+++ src/providers/generic/file/NodeFs.ts
@@ -9,14 +9,10 @@ export default class NodeFs extends FsProvider {
     private static lock = new Lock();
 
     async exists(path: string): Promise<boolean> {
-        return new Promise((resolve, reject) => {
-            NodeFs.lock.acquire(path, async () => {
-                const result = await fs.promises.access(path, fs.constants.F_OK)
-                    .then(() => true)
-                    .catch(() => false);
-                resolve(result);
-            }).catch(reject);
-        });
+        await NodeFs.lock.acquire(path, async () => {
+            const result = await fs.promises.access(path, fs.constants.F_OK).catch(return false);
+            return true;
+        }).catch(return false);
     }
 
     async stat(path: string): Promise<StatInterface> {
@@ -32,36 +28,27 @@ export default class NodeFs extends FsProvider {
     }
 
     async mkdirs(path: string): Promise<void> {
-        return new Promise((resolve, reject) => {
-            NodeFs.lock.acquire(path, async () => {
-                try {
-                    await fs.promises.mkdir(path, { recursive: true });
-                    resolve();
-                } catch (e) {
-                    reject(e);
-                }
+            return await NodeFs.lock.acquire(path, async () => {
+                return await fs.promises.mkdir(path, { recursive: true }).catch (e) {
+                throw e;
+             }
             });
-        });
     }
 
     async readFile(path: string): Promise<Buffer> {
-        return new Promise((resolve, reject) => {
-            NodeFs.lock.acquire(path, () => {
-                try {
-                    let content = fs.readFileSync(path);
-                    resolve(content);
-                } catch (e) {
-                    reject(e);
-                }
-            }).catch(reject);
-        });
+        return await NodeFs.lock.acquire(path, () => {
+                let content = fs.readFileSync(path);
+                return content;
+            }).catch (e) {
+                throw e;
+            }
     }
 
     async base64FromZip(path: string): Promise<string> {
         return (await this.readFile(path)).toString("base64");
     }
 
-    async readdir(path: string): Promise<string[]> {
+    async readdir(path: string): string[] {
         return await fs.promises.readdir(path);
     }
 
@@ -70,86 +57,66 @@ export default class NodeFs extends FsProvider {
     }
 
     async unlink(path: string): Promise<void> {
-        return new Promise((resolve, reject) => {
-            NodeFs.lock.acquire(path, async () => {
-                try {
-                    await fs.promises.unlink(path)
-                    resolve();
-                } catch (e) {
-                    reject(e)
-                }
-            }).catch(reject);
-        })
+            return await NodeFs.lock.acquire(path, async () => {
+                return await fs.promises.unlink(path);
+            }).catch(e) {
+                throw e;
+            }
     }
 
     async writeFile(path: string, content: string | Buffer): Promise<void> {
-        return new Promise((resolve, reject) => {
-            NodeFs.lock.acquire(path, () => {
+            return await NodeFs.lock.acquire(path, () => {
                 fs.writeFileSync(path, content);
-                resolve();
-            }).catch(reject);
-        })
+            }).catch(e) {
+                throw e;
+            }
     }
 
     async rename(path: string, newPath: string): Promise<void> {
-        return new Promise((resolve, reject) => {
-            NodeFs.lock.acquire([path, newPath], async () => {
-                try {
-                    await fs.promises.rename(path, newPath);
-                    resolve();
-                } catch (e) {
-                    reject(e);
-                }
-            })
-        });
+        return await NodeFs.lock.acquire([path, newPath], async () => {
+            return await fs.promises.rename(path, newPath);
+            }).catch(e) {
+                throw e
+            }
     }
 
     async chmod(path: string, mode: string | number): Promise<void> {
-        return fs.promises.chmod(path, mode);
+        return await fs.promises.chmod(path, mode);
     }
 
     async copyFile(from: string, to: string): Promise<void> {
-        return new Promise((resolve, reject) => {
-            NodeFs.lock.acquire([from, to], async () => {
-                try {
-                    await fs.promises.copyFile(from, to);
-                    resolve();
-                } catch (e) {
-                    reject(e);
-                }
-            }).catch(reject);
-        });
+        return await NodeFs.lock.acquire([from, to], async () => {
+            return await fs.promises.copyFile(from, to);
+        }).catch(e) {
+            throw e;
+        }
     }
 
     async copyFolder(from: string, to: string): Promise<void> {
-        return await this.mkdirs(to).then(() => {
-            return fs.promises.readdir(from)
-                .then(result => {
-                    result.forEach(item => {
-                        if (fs.lstatSync(path.join(from, item)).isDirectory()) {
-                            if (!fs.existsSync(path.join(to, item))) {
-                                fs.mkdirSync(path.join(to, item));
-                            }
-                            this.copyFolder(path.join(from, item), path.join(to, item));
-                        } else {
-                            this.copyFile(path.join(from, item), path.join(to, item));
+        try {
+            await this.mkdirs(to);
+            let fromDirs = await fs.promises.readdir(from);
+                fromDirs.forEach(item => {
+                    if (fs.lstatSync(path.join(from, item)).isDirectory()) {
+                        if (!fs.existsSync(path.join(to, item))) {
+                            fs.mkdirSync(path.join(to, item));
                         }
-                        return;
-                })
-            });
-        })
+                        this.copyFolder(path.join(from, item), path.join(to, item));
+                    } else {
+                        this.copyFile(path.join(from, item), path.join(to, item));
+                    }
+                    return;
+            })
+        }.catch(e) {
+            throw e;
+        }
     }
 
     async setModifiedTime(path: string, time: Date): Promise<void> {
-        return new Promise((resolve, reject) => {
-            NodeFs.lock.acquire(path, async () => {
-                try {
-                    await fs.promises.utimes(path, time, time);
-                    resolve();
-                } catch (e) {
-                    reject(e);
-                }
-            }).catch(reject);
-        });
+        return await NodeFs.lock.acquire(path, async () => {
+            return await fs.promises.utimes(path, time, time);
+        }).catch(e) {
+            throw e;
+        };
     }
 }
