961c7d735eb8345a119006baabefa41db6020e69
cade <ca469@kent.ac.uk>
2025-08-29 10:32:21 +0100

    Fixed path import in WrapperArguments.ts

--- src/components/computed/WrapperArguments.ts
+++ src/components/computed/WrapperArguments.ts
@@ -1,5 +1,5 @@
 import {computed} from "vue";
-import path from "path";
+import path from "../../providers/node/path/path";
 import PathResolver from "../../r2mm/manager/PathResolver";
 
 export const ComputedWrapperLaunchArguments = computed(() => `"${path.join(PathResolver.MOD_ROOT, process.platform === 'darwin' ? 'macos_proxy' : 'linux_wrapper.sh')}" %command%`);
