ae4f88269085c6650ffe911b3d16e69893860522
Antti Mäki <antti@anttimaki.com>
2025-09-12 10:43:22 +0300

    Add getModLoaderMapping to ThunderstoreSchema

--- src/installers/BepInExInstaller.ts
+++ src/installers/BepInExInstaller.ts
@@ -1,8 +1,7 @@
 import { InstallArgs, PackageInstaller, uninstallModLoader } from "./PackageInstaller";
 import path from "../providers/node/path/path";
 import FsProvider from "../providers/generic/file/FsProvider";
-import { MODLOADER_PACKAGES } from "../r2mm/installing/profile_installers/ModLoaderVariantRecord";
-import { PackageLoader } from "../model/schema/ThunderstoreSchema";
+import { getModLoaderMapping } from "../model/schema/ThunderstoreSchema";
 
 const basePackageFiles = ["manifest.json", "readme.md", "icon.png"];
 
@@ -17,7 +16,7 @@ export class BepInExInstaller implements PackageInstaller {
             profile,
         } = args;
 
-        const mapping = MODLOADER_PACKAGES.find((entry) => entry.packageName.toLowerCase() == mod.getName().toLowerCase());
+        const mapping = getModLoaderMapping(mod.getName());
         const mappingRoot = mapping ? mapping.rootFolder : "";
 
         let bepInExRoot: string;
