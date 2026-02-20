c1bb251134038e20f61d0273be269a00d76b20f5
Cade Ayres <ca469@kent.ac.uk>
2026-02-20 08:17:15 +0000

    Removed some redundant test code and cleared some type errors in GenericProfileInstaller.ts

--- test/vitest/tests/test-setup.ts
+++ test/vitest/tests/test-setup.ts
@@ -1,8 +1,4 @@
-import PathResolver from '../../../src/r2mm/manager/PathResolver';
 import FsProvider from '../../../src/providers/generic/file/FsProvider';
-import Profile from '../../../src/model/Profile';
-import FileUtils from '../../../src/utils/FileUtils';
-import NodeFs from '../../../src/providers/generic/file/NodeFs';
 import StubFsProvider from '../stubs/providers/stub.FsProvider';
 import StubInteractionProvider from '../stubs/providers/stub.InteractionProvider';
 import InteractionProvider from '../../../src/providers/ror2/system/InteractionProvider';
@@ -10,25 +6,8 @@ import StubLinkProvider from '../stubs/providers/stub.LinkProvider';
 import LinkProvider from '../../../src/providers/components/LinkProvider';
 import StubProfileProvider from '../stubs/providers/stub.ProfileProvider';
 import ProfileProvider from '../../../src/providers/ror2/model_implementation/ProfileProvider';
-import {providePathImplementation} from "../../../src/providers/node/path/path";
-import {TestPathProvider} from "../stubs/providers/node/Node.Path.Provider";
 
 export default class TestSetup {
-
-    public static setUp() {
-        const fs = new NodeFs();
-        FsProvider.provide(() => fs);
-        PathResolver.APPDATA_DIR = '__test_data__';
-        new Profile('Default');
-
-        providePathImplementation(() => TestPathProvider);
-    }
-
-    public static async tearDown() {
-        const fs = FsProvider.instance;
-        await FileUtils.emptyDirectory(PathResolver.APPDATA_DIR);
-        await fs.rmdir(PathResolver.APPDATA_DIR);
-    }
 
     public static stubSetUp() {
         const fs = new StubFsProvider();
