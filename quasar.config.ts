1e9e4225aed18302137d1f99310a7a2b5105eed1
Thomas <77514919+TB516@users.noreply.github.com>
2026-03-02 23:37:25 -0500

    Removed logic to build/release a standalone flatpak file

--- quasar.config.ts
+++ quasar.config.ts
@@ -1,14 +1,10 @@
 // Configuration for your app
 // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file
 
-import { defineConfig } from '#q-app/wrappers';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { defineConfig } from "#q-app/wrappers";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig((ctx) => {
-
-    const isFlatpakTarget = process.env.BUILD_FLATPAK === "true";
-    console.log("isFlatpakTarget", isFlatpakTarget);
-
     return {
         // https://v2.quasar.dev/quasar-cli-vite/prefetch-feature
         // preFetch: true,
@@ -17,15 +13,13 @@ export default defineConfig((ctx) => {
         // --> boot files are part of "main.js"
         // https://v2.quasar.dev/quasar-cli-vite/boot-files
         boot: [
-            'i18n',
+            "i18n",
             // 'axios',
-            'floating-vue'
+            "floating-vue",
         ],
 
         // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#css
-        css: [
-            'app.scss'
-        ],
+        css: ["app.scss"],
 
         // https://github.com/quasarframework/quasar/tree/dev/extras
         extras: [
@@ -37,36 +31,36 @@ export default defineConfig((ctx) => {
             // 'line-awesome',
             // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
 
-            'roboto-font', // optional, you are not bound to it
-            'material-icons', // optional, you are not bound to it
+            "roboto-font", // optional, you are not bound to it
+            "material-icons", // optional, you are not bound to it
         ],
 
         // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#build
         build: {
             target: {
-                browser: [ 'esnext' ],
-                node: 'esnext'
+                browser: ["esnext"],
+                node: "esnext",
             },
 
             typescript: {
                 strict: true,
-                vueShim: true
+                vueShim: true,
                 // extendTsConfig (tsConfig) {}
             },
 
-            vueRouterMode: 'history', // available values: 'hash', 'history'
+            vueRouterMode: "history", // available values: 'hash', 'history'
             // vueRouterBase,
             // vueDevtools,
             // vueOptionsAPI: false,
 
             rebuildCache: false, // rebuilds Vite/linter/etc cache on startup
 
-            publicPath: '/',
+            publicPath: "/",
             // analyze: true,
             // env: {},
             // rawDefine: {}
             // ignorePublicFolder: true,
-            minify: 'esbuild',
+            minify: "esbuild",
             polyfillModulePreload: true,
             // distDir
 
@@ -74,39 +68,39 @@ export default defineConfig((ctx) => {
             viteVuePluginOptions: {
                 template: {
                     compilerOptions: {
-                        isCustomElement: (tag: string) => ["strike"].includes(tag)
-                    }
-                }
+                        isCustomElement: (tag: string) =>
+                            ["strike"].includes(tag),
+                    },
+                },
             },
 
             win: {
                 publish: {
-                    provider: 'github'
-                }
+                    provider: "github",
+                },
             },
             linux: {
                 publish: {
-                    provider: 'github'
-                }
+                    provider: "github",
+                },
             },
 
-            vitePlugins: [
-            ]
+            vitePlugins: [],
         },
 
         // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#devserver
         devServer: {
             https: false,
             port: 9020,
-            open: true // opens browser window automatically
+            open: true, // opens browser window automatically
         },
 
         // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#framework
         framework: {
             config: {},
 
-            iconSet: 'material-icons', // Quasar icon set
-            lang: 'en-US', // Quasar language pack
+            iconSet: "material-icons", // Quasar icon set
+            lang: "en-US", // Quasar language pack
 
             // For special cases outside of where the auto-import strategy can have an impact
             // (like functional components as one of the examples),
@@ -116,7 +110,7 @@ export default defineConfig((ctx) => {
             // directives: [],
 
             // Quasar plugins
-            plugins: []
+            plugins: [],
         },
 
         // animations: 'all', // --- includes all animations
@@ -125,24 +119,27 @@ export default defineConfig((ctx) => {
 
         // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#sourcefiles
         sourceFiles: {
-            rootComponent: 'src/AppWrapper.vue',
+            rootComponent: "src/AppWrapper.vue",
             // router: 'src/router/index',
             // store: 'src/store/index',
             //   pwaRegisterServiceWorker: 'src-pwa/register-service-worker',
             //   pwaServiceWorker: 'src-pwa/custom-service-worker',
             //   pwaManifestFile: 'src-pwa/manifest.json',
-            electronMain: process.env.NODE_ENV === 'development' ? 'src-electron/electron-main.dev.ts' : 'src-electron/electron-main.ts',
-            electronPreload: 'src-electron/electron-preload'
+            electronMain:
+                process.env.NODE_ENV === "development"
+                    ? "src-electron/electron-main.dev.ts"
+                    : "src-electron/electron-main.ts",
+            electronPreload: "src-electron/electron-preload",
             //   bexManifestFile: 'src-bex/manifest.json
         },
 
         // https://v2.quasar.dev/quasar-cli-vite/developing-ssr/configuring-ssr
         ssr: {
             prodPort: 3000, // The default port that the production server should use
-                            // (gets superseded if process.env.PORT is specified at runtime)
+            // (gets superseded if process.env.PORT is specified at runtime)
 
             middlewares: [
-                'render' // keep this as last one
+                "render", // keep this as last one
             ],
 
             // extendPackageJson (json) {},
@@ -153,7 +150,7 @@ export default defineConfig((ctx) => {
             // manualStoreHydration: true,
             // manualPostHydrationTrigger: true,
 
-            pwa: false
+            pwa: false,
             // pwaOfflineHtmlFilename: 'offline.html', // do NOT use index.html as name!
 
             // pwaExtendGenerateSWOptions (cfg) {},
@@ -162,7 +159,7 @@ export default defineConfig((ctx) => {
 
         // https://v2.quasar.dev/quasar-cli-vite/developing-pwa/configuring-pwa
         pwa: {
-            workboxMode: 'GenerateSW' // 'GenerateSW' or 'InjectManifest'
+            workboxMode: "GenerateSW", // 'GenerateSW' or 'InjectManifest'
             // swFilename: 'sw.js',
             // manifestFilename: 'manifest.json',
             // extendManifestJson (json) {},
@@ -180,109 +177,63 @@ export default defineConfig((ctx) => {
 
         // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-capacitor-apps/configuring-capacitor
         capacitor: {
-            hideSplashscreen: true
+            hideSplashscreen: true,
         },
 
         // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-electron-apps/configuring-electron
         electron: {
-
             // extendElectronMainConf (esbuildConf) {},
             // extendElectronPreloadConf (esbuildConf) {},
 
             // Electron preload scripts (if any) from /src-electron, WITHOUT file extension
-            preloadScripts: [ 'electron-preload' ],
+            preloadScripts: ["electron-preload"],
 
             // specify the debugging port to use for the Electron app when running in development mode
             inspectPort: 5858,
 
-            bundler: 'builder', // 'packager' or 'builder'
+            bundler: "builder", // 'packager' or 'builder'
 
             packager: {
                 // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
-
                 // OS X / Mac App Store
                 // appBundleId: '',
                 // appCategoryType: '',
                 // osxSign: '',
                 // protocol: 'myapp://path',
-
                 // Windows only
                 // win32metadata: { ... }
             },
 
             builder: {
-                // https://www.electron.build/configuration/configuration
+                // https://www.electron.build/configuration/
 
-                appId: isFlatpakTarget ? 'com.github.ebkr.r2modman' : 'ebkr-r2modman',
-                compression: 'store',
-
-                flatpak: {
-                    runtimeVersion: "25.08",
-                    baseVersion: "25.08",
-                    finishArgs: [
-                        // Access to flatpak-spawn
-                        "--talk-name=org.freedesktop.Flatpak.Development",
-
-                        // Filesystem full access
-                        "--filesystem=home",
-                        "--filesystem=/mnt",
-                        "--filesystem=host",
-                        "--filesystem=~/.var/app/com.valvesoftware.Steam:rw",
-                        "--filesystem=~/.local/share/Steam:rw",
-
-                        // Allows the manager to use IPC - Useful to prevent multiple instances
-                        "--share=ipc",
-
-                        // Display server permissions
-                        "--socket=x11",
-                        "--socket=wayland",
-
-                        // IO
-                        "--socket=session-bus",
-                        "--device=all",
-
-                        // Network access
-                        "--share=network",
-
-                        // Additional requirements for Steam
-                        "--socket=pulseaudio",
-                        "--socket=system-bus",
-                    ],
-
-                },
+                appId: "ebkr-r2modman",
+                compression: "store",
 
                 win: {
-                    target: ['nsis', 'portable'],
-                    icon: 'src/assets/icon.ico'
+                    target: ["nsis", "portable"],
+                    icon: "src/assets/icon.ico",
                 },
                 nsis: {
                     oneClick: false,
                     allowToChangeInstallationDirectory: true,
                     allowElevation: false,
                     perMachine: false,
-                    include: 'build/installer.nsh'
+                    include: "build/installer.nsh",
                 },
                 linux: {
-                    target: (() => {
-                        if (isFlatpakTarget) {
-                            return ['flatpak']
-                        } else {
-                            return ['AppImage', 'tar.gz', 'deb', 'rpm', 'pacman']
-                        }
-                    })(),
-                    icon: 'src/assets/icon',
-                    maintainer: 'ebkr',
-                    vendor: 'ebkr',
-                    synopsis: 'Risk of Rain 2 Mod Manager',
-                    category: 'Game',
-                    mimeTypes: [
-                        "x-scheme-handler/ror2mm"
-                    ],
+                    target: ["AppImage", "tar.gz", "deb", "rpm", "pacman"],
+                    icon: "src/assets/icon",
+                    maintainer: "ebkr",
+                    vendor: "ebkr",
+                    synopsis: "Risk of Rain 2 Mod Manager",
+                    category: "Game",
+                    mimeTypes: ["x-scheme-handler/ror2mm"],
                 },
                 mac: {
                     category: "games",
-                    icon: "src/assets/icon"
-                }
+                    icon: "src/assets/icon",
+                },
             },
 
             nodeIntegration: true,
@@ -301,7 +252,7 @@ export default defineConfig((ctx) => {
              *
              * @example [ 'my-script.ts', 'sub-folder/my-other-script.js' ]
              */
-            extraScripts: []
-        }
-    }
+            extraScripts: [],
+        },
+    };
 });
