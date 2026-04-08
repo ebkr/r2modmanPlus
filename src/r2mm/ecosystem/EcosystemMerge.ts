685e42156881136e927bd73ff0ff4ede602d9836
cade <cade.ayres@riskidev.fi>
2026-04-08 10:28:22 +0100

    Added implementation to allow extending static json schema

--- src/r2mm/ecosystem/EcosystemMerge.ts
+++ src/r2mm/ecosystem/EcosystemMerge.ts
@@ -1,3 +1,100 @@
-export async function generateMergedSchema(): Promise<void> {
-    throw new Error("Not implemented.");
+import bundledEcosystem from "../../assets/data/ecosystem.json";
+import {R2Modman, ThunderstoreEcosystem} from "../../assets/data/ecosystemTypes";
+import jsonSchema from "../../assets/data/ecosystemJsonSchema.json";
+import R2Error from "../../model/errors/R2Error";
+import Ajv from "ajv";
+import addFormats from "ajv-formats";
+import PathResolver from "../manager/PathResolver";
+import path from "../../providers/node/path/path";
+import FsProvider from "../../providers/generic/file/FsProvider";
+import VersionNumber from "../../model/VersionNumber";
+import ManagerInformation from "../../_managerinf/ManagerInformation";
+import {EcosystemModloaderPackages, EcosystemSupportedGames} from "../../model/schema/ThunderstoreSchema";
+
+export type MergedThunderstoreEcosystem = ThunderstoreEcosystem & {version: string};
+
+async function getMergedEcosystemPath(): Promise<string> {
+    return path.join(PathResolver.MOD_ROOT, "ecosystem-merge.json");
+}
+
+async function updateLatestMergedEcosystemSchema(): Promise<void> {
+    const bundledSchema = await loadBundledSchema();
+    const latestSchema = await fetchLatestSchema();
+    const mergedSchema: ThunderstoreEcosystem = {
+        schemaVersion: "",
+        communities: {},
+        games: {},
+        modloaderPackages: [],
+        packageInstallers: {},
+    };
+    mergedSchema.schemaVersion = latestSchema.schemaVersion;
+    mergedSchema.communities = {
+        ...bundledSchema.communities,
+        ...latestSchema.communities,
+    }
+    mergedSchema.games = {
+        ...bundledSchema.games,
+        ...latestSchema.games,
+    };
+    // Convert to map to handle duplicate keys nicely
+    const modloaderMap = new Map(
+        [...bundledSchema.modloaderPackages, ...latestSchema.modloaderPackages]
+            .map(pkg => [pkg.packageId, pkg])
+    );
+    mergedSchema.modloaderPackages = [...modloaderMap.values()];
+    mergedSchema.packageInstallers = {
+        ...bundledSchema.packageInstallers,
+        ...latestSchema.packageInstallers,
+    };
+}
+
+async function getLastSavedMergedEcosystemSchema(): Promise<MergedThunderstoreEcosystem> {
+    const contentBuffer = await FsProvider.instance.readFile(await getMergedEcosystemPath());
+    const content = contentBuffer.toString("utf8");
+    return JSON.parse(content);
+}
+
+async function loadBundledSchema(): Promise<ThunderstoreEcosystem> {
+    const ajv = new Ajv();
+    addFormats(ajv);
+
+    const validate = ajv.compile(jsonSchema);
+    const isOk = validate(bundledEcosystem);
+
+    if (!isOk) {
+        throw new R2Error("Schema validation error", ajv.errorsText(validate.errors));
+    }
+
+    return bundledEcosystem as ThunderstoreEcosystem;
+}
+
+async function fetchLatestSchema(): Promise<ThunderstoreEcosystem> {
+    // TODO - Implement fetching of latest resource
+    return {} as ThunderstoreEcosystem;
+}
+
+async function resolveMergedEcosystemSchema(): Promise<MergedThunderstoreEcosystem> {
+    const mergeFilePath = await getMergedEcosystemPath();
+    const bundledSchema = async () => ({...(await loadBundledSchema()), version: ManagerInformation.VERSION.toString()});
+    if (!(await FsProvider.instance.exists(mergeFilePath))) {
+        return bundledSchema();
+    }
+    const content = await getLastSavedMergedEcosystemSchema();
+    if (!new VersionNumber(content.version).isEqualTo(ManagerInformation.VERSION)) {
+        return bundledSchema();
+    }
+    return content;
+}
+
+export async function updateEcosystemReactives() {
+    const mergedSchema = await resolveMergedEcosystemSchema();
+    const result: [string, R2Modman][] = []
+    for (const [identifier, game] of Object.entries(mergedSchema.games)) {
+        if (game.r2modman == null) continue;
+        for (const entry of game.r2modman) {
+            result.push([identifier, entry]);
+        }
+    }
+    EcosystemSupportedGames.value = result;
+    EcosystemModloaderPackages.value = mergedSchema.modloaderPackages;
 }
