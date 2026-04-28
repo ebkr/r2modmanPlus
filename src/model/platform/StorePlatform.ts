0492fe1aafac3c83ad233cb870a3465c5778674e
cade <cade.ayres@riskidev.fi>
2026-04-29 07:11:50 +0800

    Renamed "Origin / EA Desktop" to "EA App"

--- src/model/platform/StorePlatform.ts
+++ src/model/platform/StorePlatform.ts
@@ -5,7 +5,7 @@ export const StorePlatform = {
     [Platform.STEAM_DIRECT]: "Steam",
     [Platform.EPIC_GAMES_STORE]: "Epic Games Store",
     [Platform.OCULUS_STORE]: "Oculus Store",
-    [Platform.ORIGIN]: "Origin / EA Desktop",
+    [Platform.ORIGIN]: "EA App",
     [Platform.XBOX_GAME_PASS]: "Xbox Game Pass",
     [Platform.OTHER]: "Other"
 }
