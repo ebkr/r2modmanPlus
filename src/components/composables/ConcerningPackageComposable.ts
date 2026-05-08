b31349b5a33fc111e31874bf17835f0ccf9afc5a
cade <cade.ayres@riskidev.fi>
2026-05-08 11:37:28 +0800

    WIP: Correctly show unlinked versions.
    - Slow

--- src/components/composables/ConcerningPackageComposable.ts
+++ src/components/composables/ConcerningPackageComposable.ts
@@ -1,25 +1,67 @@
 import { getStore } from '@r2/providers/generic/store/StoreProvider';
-import { computed, ref, watch } from 'vue';
+import { computed, onMounted, ref, watch } from 'vue';
 import ManifestV2 from '@r2/model/ManifestV2';
 import ThunderstoreMod from '@r2/model/ThunderstoreMod';
+import VersionNumber from '@r2/model/VersionNumber';
+import * as PackageDb from "@r2/r2mm/manager/PackageDexieStore";
+
+type ConcerningPackage = {
+    fullName: string;
+    latestVersion: string;
+}
 
 const store = getStore<any>();
 
+const activeGame = computed(() => store.state.activeGame);
+
 const localModList = computed<ManifestV2[]>(() => store.state.profile.modList);
-const onlineModList = computed<Map<string, ThunderstoreMod>>(() => {
+const onlineModList = computed<Map<string, ConcerningPackage>>(() => {
     const mods: ThunderstoreMod[] = store.state.tsMods.mods;
-    return new Map<string, ThunderstoreMod>(mods.map(value => [value.getFullName(), value]));
+    return new Map<string, ConcerningPackage>(mods.map(value => [value.getFullName(), {
+        fullName: value.getFullName(),
+        latestVersion: value.getLatestVersion(),
+    }]));
 });
 
-const allConcerningPackages = computed<ManifestV2[]>(() => {
-    return localModList.value.filter(value => (value.isOnlineSource() && !onlineModList.value.has(value.getName())));
-});
+const allConcerningPackages = ref<ManifestV2[]>([]);
+const activeConcerningPackages = ref<ManifestV2[]>([]);
 
-const activeConcerningPackages = computed<ManifestV2[]>(() => {
-    return allConcerningPackages.value.filter(value => !value.isTrustedPackage() && (value.isOnlineSource() && !onlineModList.value.has(value.getName())));
+async function updateConcerningPackages() {
+    const game = activeGame.value;
+    const localMods = localModList.value;
+    const concerningPackages = [];
+
+    for (const mod of localMods) {
+        if (!mod.isOnlineSource()) {
+            continue;
+        }
+        if (!onlineModList.value.has(mod.getName())) {
+            concerningPackages.push(mod);
+            continue;
+        }
+        const packageVersionNumbers = await PackageDb.getPackageVersionNumbers(
+            game.internalFolderName,
+            mod.getName()
+        );
+        const dnf = !packageVersionNumbers.find(ver => ver === mod.getVersionNumber().toString());
+        if (dnf) {
+            concerningPackages.push(mod);
+        }
+    }
+
+    allConcerningPackages.value = concerningPackages;
+    activeConcerningPackages.value = concerningPackages.filter(value => !value.isTrustedPackage());
+}
+
+watch([activeGame, localModList], async () => {
+    await updateConcerningPackages();
 });
 
 export function useConcerningPackageComposable() {
+
+    onMounted(async () => {
+        await updateConcerningPackages();
+    });
 
     const hasConcerningPackages = computed<boolean>(() => activeConcerningPackages.value.length > 0);
 
