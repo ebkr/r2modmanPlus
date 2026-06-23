7e57e72fd3a9f463d16cd54ea9e7a84e57d78a9e
Cade Ayres <cade.ayres@riskidev.fi>
2026-06-23 21:45:48 +0800

    Removed now-redundant prewarmCache call (#2212)

--- src/store/modules/TsModsModule.ts
+++ src/store/modules/TsModsModule.ts
@@ -250,7 +250,6 @@ export const TsModsModule = {
                     await dispatch('updateMods');
                     commit('setThunderstoreModListUpdateStatus', 'Almost done...');
                     await dispatch('profile/tryLoadModListFromDisk', null, {root: true});
-                    await dispatch('prewarmCache');
                 }
             } catch (e) {
                 commit('setThunderstoreModListUpdateError', e);
@@ -361,11 +360,6 @@ export const TsModsModule = {
             }
 
             return state.activeGameCacheStatus || 'Unknown status';
-        },
-
-        async prewarmCache({rootGetters, commit}) {
-            const profileMods: ManifestV2[] = rootGetters['profile/modList'];
-            commit('prewarmCacheMod', profileMods);
         },
 
         async pruneRemovedModsFromCache({rootState}, cutoff: Date) {
