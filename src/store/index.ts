9c3b28f010cd59fd5a638b767b1e6a49211e19ce
Cade Ayres <cade.ayres@riskidev.fi>
2026-07-03 10:53:15 +0800

    Restored dev mode in store/index.ts (#2235)

--- src/store/index.ts
+++ src/store/index.ts
@@ -140,7 +140,7 @@ export const store = {
 
     // enable strict mode (adds overhead!)
     // for dev mode only
-    strict: import.meta.env.MODE === 'developmen',
+    strict: import.meta.env.MODE === 'development',
 };
 
 /*
