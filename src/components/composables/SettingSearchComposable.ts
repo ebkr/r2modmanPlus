3305acf14ed02ba4dffd9a0092bb4e0bd5d1cdaa
cade <cade.ayres@riskidev.fi>
2026-07-14 14:22:01 +0800

    Simplified SettingSearchComposable searching

--- src/components/composables/SettingSearchComposable.ts
+++ src/components/composables/SettingSearchComposable.ts
@@ -4,20 +4,27 @@ import SearchUtils from '../../utils/SearchUtils';
 
 export type SettingSearchPhrases = MaybeRefOrGetter<(string | undefined | null)[]>;
 
+function isNonEmptyPhrase(phrase: string | undefined | null): phrase is string {
+    return phrase !== null && phrase !== undefined && phrase !== '';
+}
+
 export function useSettingSearch(
     searchTerm: MaybeRefOrGetter<string | undefined>,
     keyPhrases: SettingSearchPhrases
 ): { isVisible: ComputedRef<boolean> } {
     const isVisible = computed<boolean>(() => {
-        const term = toValue(searchTerm);
-        if (!term) {
+        const activeSearchTerm = toValue(searchTerm);
+        if (!activeSearchTerm) {
             return true;
         }
-        const keys = SearchUtils.makeKeys(term);
-        const haystack = toValue(keyPhrases)
-            .filter((phrase): phrase is string => Boolean(phrase))
-            .join(' ');
-        return SearchUtils.isSearched(keys, haystack);
+
+        const searchKeys = SearchUtils.makeKeys(activeSearchTerm);
+
+        const resolvedPhrases = toValue(keyPhrases);
+        const nonEmptyPhrases = resolvedPhrases.filter(isNonEmptyPhrase);
+        const searchableText = nonEmptyPhrases.join(' ');
+
+        return SearchUtils.isSearched(searchKeys, searchableText);
     });
 
     return { isVisible };
